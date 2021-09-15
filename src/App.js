15561211b5fe082375e0cb47ba467dd46c1a40cf
Mauro Vera <Maurovera069@gmail.com>
2021-09-15 15:17:22 +0200

    Limpiando codigo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import NavBar from './components/NavBar';
-import HomeContainer from './Containers/HomeContainer';
-import NotFound from './components/NotFound';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import HomeContainer from "./Containers/HomeContainer";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar></NavBar>
       <Switch>
-        <Route exact path ="/" component ={HomeContainer}/>
-        <Route path = "" component = {NotFound}/> 
+        <Route exact path="/" component={HomeContainer} />
+        <Route path="" component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
