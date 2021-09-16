3ed8e3c57542af25a07a76de0b25d8b2d5976852
spancavil <soancavil@gmail.com>
2021-09-16 10:43:47 -0300

    Social-media buttons and landing home init

---
+++ src/Components/SocialMedia/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SvgComponent from '../../Assets/svg/faceVector'
+import styles from './styles.module.scss'
+
+const SocialMedia = () => {
+    return (
+        <div className={styles.socialMedia}>
+            <SvgComponent/>
+            
+        </div>
+    )
+}
+
+export default SocialMedia
--- src/Views/Home/index.jsx
+++ src/Views/Home/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styles from './styles.module.scss'
+import backImg from '../../Assets/img/bg-header-1.png';
+import bpBrand from '../../Assets/img/BI-BP-2.png'
+import SocialMedia from '../../Components/SocialMedia';
 
 const HomeContainer = () => {
+    console.log(backImg)
     return (
-        <div>
-            <h1>Este es el Home</h1>
+        <div className = {styles.home}
+        style={{backgroundImage: `url(${backImg})`}}>
+            <h3>Soy el home</h3>
+            <SocialMedia/>
+            <img src={bpBrand} alt=" bp-brand"/>
         </div>
     )
 }
