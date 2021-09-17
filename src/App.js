897c5df8b592ef0f027001eb88775894c1d855b3
Mauro Vera <Maurovera069@gmail.com>
2021-09-17 16:35:39 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomeContainer from "./Views/Home";
 import NavBar from "./Views/Navbar";
 import NotFound from "./Views/NotFound";
-
-import "./Styles/Global.scss";
 import Account from "./Views/Account";
+import SignUp from "./Views/SignUp";
+import "./Styles/Global.scss";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <NavBar />
       <Switch>
         <Route exact path="/" component={HomeContainer} />
+        <Route exact path="/signup" component={SignUp} />
         <Route exact path="/account" component={Account} />
         <Route path="" component={NotFound} />
       </Switch>
