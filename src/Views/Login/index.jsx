6afa9a59937b89bfb2b40ee119b1c64554e16658
Mauro Vera <Maurovera069@gmail.com>
2021-09-20 12:15:45 -0300

    Cree estados para login

--- src/Views/Login/index.jsx
+++ src/Views/Login/index.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '../../Global-Components/Button';
 import Input from '../../Global-Components/Input';
 import Modal from '../../Global-Components/Modal';
 import SubMessage from '../../Global-Components/SubMessage';
+import styles from './styles.module.scss';
 
 const Login = () => {
+  const [email, setEmail] = useState ('');
+
+  const changeEmail = email => {
+    setEmail (email);
+  };
+
   return (
-    <Modal title="LOGIN">
-      <Input label="Email" width="32vw" type="email" />
-      <div style={{paddingTop: '25px'}}>
-        <Button title="GET CODE" />
+    <div className={styles.container}>
+      <div className={styles.image}>
+        <Modal title="LOGIN">
+          <Input
+            label="Email"
+            width="32vw"
+            type="email"
+            handleChange={email => changeEmail (email)}
+          />
+          <div style={{paddingTop: '25px'}}>
+            <Button title="GET CODE" />
+          </div>
+          <SubMessage
+            text="Do not have an account?"
+            link="/signup"
+            textLink="Sign up"
+          />
+        </Modal>
       </div>
-      <SubMessage
-        text="Do not have an account?"
-        link="/signup"
-        textLink="Sign up"
-      />
-    </Modal>
+    </div>
   );
 };
 
