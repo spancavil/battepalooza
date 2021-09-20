d0fc37d12d6e3c4b21de861238df0d002095d918
Mauro Vera <Maurovera069@gmail.com>
2021-09-20 11:15:51 -0300

    Cree la ruta Login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./Views/Navbar";
 import NotFound from "./Views/NotFound";
 import Account from "./Views/Account";
 import SignUp from "./Views/SignUp";
+import Login from "./Views/Login";
 import "./Styles/Global.scss";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomeContainer} />
         <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/account" component={Account} />
         <Route path="" component={NotFound} />
       </Switch>
