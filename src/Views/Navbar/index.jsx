583f4a392e4bcbc995183c5cae49b54ce93c9675
Mauro Vera <Maurovera069@gmail.com>
2021-09-30 15:50:07 -0300

    Arregle lo de la id

--- src/Views/Navbar/index.jsx
+++ src/Views/Navbar/index.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
 
   useEffect (
     () => {
-      userData._id && setId (userData._id.slice (0, 4));
+      userData._id && setId (userData._id.slice (-4));
     },
     [userData]
   );
