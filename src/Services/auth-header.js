58afd305f40888bd49866c9df38f97c98bcd69a1
sancavillwaypoint <sancavil@waypoint.la>
2021-09-29 22:47:08 -0300

    Set userData and userToken in context. Change burger on NavBar

--- src/Services/auth-header.js
+++ src/Services/auth-header.js
@@ -1,9 +1,7 @@
-export default function authHeader() {
-    const user = JSON.parse(localStorage.getItem('user'));
-  
-    if (user && user.token) {
-      return { Authorization: 'Bearer ' + user.token };
-    } else {
-      return {};
-    }
+export default function authHeader(token) {
+  if (token) {
+    return { Authorization: 'Bearer ' + token };
+  } else {
+    return {};
   }
+}
