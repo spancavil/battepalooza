ac74bf38fc3a237bfafcd9279b278731f493ab9c
Mauro Vera <Maurovera069@gmail.com>
2021-10-04 16:26:09 -0300

    Packs component

--- src/Views/Packs/index.jsx
+++ src/Views/Packs/index.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import styles from './styles.module.scss'
+import React from 'react';
+import Background from '../../Global-Components/Background';
+import SocialMedia from '../Home/Components/SocialMedia';
+import styles from './styles.module.scss';
 
 const Packs = () => {
-    return (
-        <div >
-            
+  return (
+    <Background>
+      <div className={styles.packContainer}>
+        <div className={styles.banner}>
+          BANNER IMG
         </div>
-    )
-}
+        <h4>
+          SKINS PACKS
+        </h4>
+        <SocialMedia />
+      </div>
+    </Background>
+  );
+};
 
-export default Packs
+export default Packs;
