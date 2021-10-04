7ce9330bec7a5addeb4af3cad93da0be2bee22da
sancavillwaypoint <sancavil@waypoint.la>
2021-10-04 15:07:57 -0300

    initial commit

---
+++ src/Global-Components/Card/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const Card = () => {
+    return(
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+
+export default Card;
