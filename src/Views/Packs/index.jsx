37f552e1eef483159b66d02774d5af2b6d8ae344
Mauro Vera <Maurovera069@gmail.com>
2021-10-04 15:23:34 -0300

    Create component Packs

---
+++ src/Views/Packs/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+const Packs = () => {
+    return (
+        <div >
+            
+        </div>
+    )
+}
+
+export default Packs
