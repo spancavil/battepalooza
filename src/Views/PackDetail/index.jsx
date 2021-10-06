e09e0ed180d015ae07219ca8a6da2e118de854de
Mauro Vera <Maurovera069@gmail.com>
2021-10-06 12:18:10 -0300

    Componente PackDetail

--- src/Views/PackDetail/index.jsx
+++ src/Views/PackDetail/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {useParams} from 'react-router';
+import {CardData} from '../../Context/CardDataProvider';
+import Background from '../../Global-Components/Background';
+import SocialMedia from '../Home/Components/SocialMedia';
+import CardDetail from './Components/CardDetail';
 import styles from './styles.module.scss';
 
 const PackDetail = () => {
-  return <div />;
+  return (
+    <Background>
+      <div className={styles.container}>
+        <CardDetail />
+      </div>
+      <SocialMedia />
+    </Background>
+  );
 };
 
 export default PackDetail;
