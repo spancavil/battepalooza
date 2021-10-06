31fc44ef2e03427050610aae5a3fe2d045a22fcb
sancavillwaypoint <sancavil@waypoint.la>
2021-10-06 10:32:03 -0300

    Pack

---
+++ src/Context/packModel.js
@@ -0,0 +1,14 @@
+const pack = {
+    id: 1,
+    imgSrc: image1,
+    description : {
+        text1: "Rare Series 1 Release",
+        text2: "1 - DROP 1",
+        text3: "2,00 PACK",
+    },
+    content: "1 rare skin 2 common skin",
+    price: 100000,
+    stock: 300,
+    soldOut: false,
+    sale: true,
+}
