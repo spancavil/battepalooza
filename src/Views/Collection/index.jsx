dd744ac2c690d85e69ff957c4fe372c294b7f49b
Mauro Vera <Maurovera069@gmail.com>
2021-10-13 17:14:41 -0300

    Update index.jsx

--- src/Views/Collection/index.jsx
+++ src/Views/Collection/index.jsx
@@ -1,42 +1,36 @@
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import Background from '../../Global-Components/Background';
 import Button from '../../Global-Components/Button';
 import styles from './styles.module.scss';
 import CollectionPacks from './Components/CollectionPacks';
 import CollectionNfts from './Components/CollectionNfts';
-import { useMediaQuery } from '../../Hooks/useMediaQuery';
-import ScrollBar from '../../Global-Components/ScrollBar';
-import { CardData } from '../../Context/CardDataProvider';
 
 const Collection = () => {
-
   return (
     <Background>
       <div className={styles.container}>
         <div className={styles.content}>
           <p>PACK</p>
           <div className={styles.rectangle}>
-            <div className={styles.cards}>
-              <CollectionPacks />
-            </div>
+            <CollectionPacks />
             <div className={styles.link}>
-            <Link to="/collection/packs">
-              <Button title="FULL LIST" />
-            </Link>
+              <Link to="/collection/packs">
+                <Button title="FULL LIST" />
+              </Link>
             </div>
           </div>
         </div>
 
-        <div className={styles.content}>
+        <div className={styles.content2}>
           <p>NFT</p>
           <div className={styles.rectangle}>
-            <div className={styles.cards}>
-              <CollectionNfts />
+            <CollectionNfts />
+            <div className={styles.link}>
+              <Link to="/collection/nft">
+                <Button title="FULL LIST" />
+              </Link>
             </div>
-            <Link to="/collection/nft">
-              <Button title="FULL LIST" />
-            </Link>
           </div>
         </div>
       </div>
