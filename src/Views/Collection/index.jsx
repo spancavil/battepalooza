9012b321fde7e2965fafb6fd3717f4e1317b5bc3
Mauro Vera <Maurovera069@gmail.com>
2021-10-13 15:04:41 -0300

    Collection component

--- src/Views/Collection/index.jsx
+++ src/Views/Collection/index.jsx
@@ -1,15 +1,15 @@
-import React, {useContext} from 'react';
-import { Link } from 'react-router-dom';
-import {CardData} from '../../Context/CardDataProvider';
+import React, { useContext, useState } from 'react';
+import {Link} from 'react-router-dom';
 import Background from '../../Global-Components/Background';
 import Button from '../../Global-Components/Button';
-import Card from '../../Global-Components/Card';
 import styles from './styles.module.scss';
+import CollectionPacks from './Components/CollectionPacks';
+import CollectionNfts from './Components/CollectionNfts';
+import { useMediaQuery } from '../../Hooks/useMediaQuery';
+import ScrollBar from '../../Global-Components/ScrollBar';
+import { CardData } from '../../Context/CardDataProvider';
 
 const Collection = () => {
-  const {packs, setPack} = useContext (CardData);
-
-  console.log (packs);
 
   return (
     <Background>
@@ -18,22 +18,13 @@ const Collection = () => {
           <p>PACK</p>
           <div className={styles.rectangle}>
             <div className={styles.cards}>
-              {packs.map (pack => {
-                return (
-                  <div className={styles.card} key={pack.id}>
-                    <img src={pack.imgSrc} alt={pack.imgSrc} />
-                    <div className={styles.texts}>
-                      <p className={styles.text1}>{pack.description.text1} </p>
-                      <p className={styles.text2}>{pack.description.text2}</p>
-                      <p className={styles.text3}>{pack.description.text3} </p>
-                    </div>
-                  </div>
-                );
-              })}
+              <CollectionPacks />
             </div>
-            <Link to='/collection/packs'>
+            <div className={styles.link}>
+            <Link to="/collection/packs">
               <Button title="FULL LIST" />
             </Link>
+            </div>
           </div>
         </div>
 
@@ -41,20 +32,9 @@ const Collection = () => {
           <p>NFT</p>
           <div className={styles.rectangle}>
             <div className={styles.cards}>
-              {packs.map (pack => {
-                return (
-                  <div className={styles.card} key={pack.id}>
-                    <img src={pack.imgSrc} alt={pack.imgSrc} />
-                    <div className={styles.texts}>
-                      <p className={styles.text1}>{pack.description.text1} </p>
-                      <p className={styles.text2}>{pack.description.text2}</p>
-                      <p className={styles.text3}>{pack.description.text3} </p>
-                    </div>
-                  </div>
-                );
-              })}
+              <CollectionNfts />
             </div>
-            <Link to='/collection/nft'>
+            <Link to="/collection/nft">
               <Button title="FULL LIST" />
             </Link>
           </div>
