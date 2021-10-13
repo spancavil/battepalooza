87109522cf0805fd7486f4a98d2f0d41a2109f7c
Mauro Vera <Maurovera069@gmail.com>
2021-10-13 15:03:14 -0300

    Add NftProvider

--- src/App.js
+++ src/App.js
@@ -18,31 +18,34 @@ import Collection from "./Views/Collection";
 import MyPacks from "./Views/MyPacks";
 import MyNft from "./Views/MyNft";
 import CardAnimation from "./Views/CardAnimation";
+import NftProvider from "./Context/NftProvider";
 
 function App() {
   return (
     <BrowserRouter>
       <UserProvider>
-        <CardDataProvider>
-          <NavBar />
-          <Switch>
-            <Route exact path="/" component={HomeContainer} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/verification" component={Verification} />
-            <Route exact path="/needlogin" component={NeedLogin} />
-            <Route exact path="/account" component={Account} />
-            <Route exact path="/packs" component={Packs} />
-            <Route exact path="/packs/:id" component={PackDetail} />
-            <Route exact path="/join-drop" component={JoinDrop} />
-            <Route exact path="/open-pack" component={OpenPack} />
-            <Route exact path="/collection" component={Collection} />
-            <Route exact path="/collection/packs" component={MyPacks} />
-            <Route exact path="/collection/nft" component={MyNft} />
-            <Route exact path="/card-animation" component={CardAnimation} />
-            <Route path="" component={NotFound} />
-          </Switch>
-        </CardDataProvider>
+        <NftProvider>
+          <CardDataProvider>
+            <NavBar />
+            <Switch>
+              <Route exact path="/" component={HomeContainer} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/verification" component={Verification} />
+              <Route exact path="/needlogin" component={NeedLogin} />
+              <Route exact path="/account" component={Account} />
+              <Route exact path="/packs" component={Packs} />
+              <Route exact path="/packs/:id" component={PackDetail} />
+              <Route exact path="/join-drop" component={JoinDrop} />
+              <Route exact path="/open-pack" component={OpenPack} />
+              <Route exact path="/collection" component={Collection} />
+              <Route exact path="/collection/packs" component={MyPacks} />
+              <Route exact path="/collection/nft" component={MyNft} />
+              <Route exact path="/card-animation" component={CardAnimation} />
+              <Route path="" component={NotFound} />
+            </Switch>
+          </CardDataProvider>
+        </NftProvider>
       </UserProvider>
     </BrowserRouter>
   );
