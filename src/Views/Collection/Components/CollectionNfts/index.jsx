29048e0cf2e5778516cefc6d3205324246289914
Mauro Vera <Maurovera069@gmail.com>
2021-10-13 17:13:54 -0300

    CollectionNft component

--- src/Views/Collection/Components/CollectionNfts/index.jsx
+++ src/Views/Collection/Components/CollectionNfts/index.jsx
@@ -1,24 +1,43 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import styles from './styles.module.scss';
-import { NftData } from '../../../../Context/NftProvider';
+import {NftData} from '../../../../Context/NftProvider';
+import { useMediaQuery } from '../../../../Hooks/useMediaQuery';
+import ScrollBar from '../../../../Global-Components/ScrollBar';
 
 const CollectionNfts = () => {
-    const {nfts, setNfts} = useContext (NftData);
+  const [scroll, setScroll] = useState ({scrollLeft: '', scrollWidth: ''});
+  const queryTablet = useMediaQuery ('(max-width: 766px)');
+  const {nfts, setNfts} = useContext (NftData);
+
+  const handleScroll = e => {
+    setScroll ({
+      scrollLeft: e.target.scrollLeft,
+      scrollWidth: e.target.scrollWidth - e.target.offsetWidth,
+    });
+  };
 
   return (
-    <>
-      {nfts.map (nft => {
-        return (
-          <div className={styles.cardNft} key={nft.id}>
-            <img className={styles.imgNft} src={nft.imgSrc} alt="nft" />
-            <div className={styles.texts}>
-              <p className={styles.text1}>Series 1 </p>
-              <p className={styles.text2}>Tron Warrior</p>
+    <div className={styles.cardsContainer}>
+      <div className={styles.cards} onScroll={handleScroll}>
+        {nfts.map (nft => {
+          return (
+            <div className={styles.cardNft} key={nft.id}>
+              <img className={styles.imgNft} src={nft.imgSrc} alt="nft" />
+              <div className={styles.texts}>
+                <p >Series 1 </p>
+                <p className={styles.text2}>Tron Warrior</p>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </>
+          );
+        })}
+      </div>
+      {queryTablet &&
+        <ScrollBar
+          width={scroll.scrollWidth}
+          position={scroll.scrollLeft}
+          elements={nfts.length}
+        />}
+    </div>
   );
 };
 
