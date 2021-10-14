7439f516fe146045b1044a641abaa7e425aa02c3
Mauro Vera <Maurovera069@gmail.com>
2021-10-14 16:36:24 -0300

    Add two props to the component

--- src/Views/Collection/Components/CollectionPacks/index.jsx
+++ src/Views/Collection/Components/CollectionPacks/index.jsx
@@ -4,7 +4,7 @@ import {CardData} from '../../../../Context/CardDataProvider';
 import {useMediaQuery} from '../../../../Hooks/useMediaQuery';
 import ScrollBar from '../../../../Global-Components/ScrollBar';
 
-const CollectionPacks = () => {
+const CollectionPacks = ({flex, short}) => {
   const [scroll, setScroll] = useState ({scrollLeft: '', scrollWidth: ''});
   const queryTablet = useMediaQuery ('(max-width: 766px)');
   const {packs} = useContext (CardData);
@@ -16,21 +16,39 @@ const CollectionPacks = () => {
     });
   };
 
+  const packsShort = packs.slice(0, 3)
+
   return (
     <div className={styles.cardsContainer}>
-      <div className={styles.cards} onScroll={handleScroll}>
-        {packs.map (pack => {
-          return (
-            <div className={styles.card} key={pack.id}>
-              <img src={pack.imgSrc} alt={pack.imgSrc} />
-              <div className={styles.texts}>
-                <p className={styles.text1}>{pack.description.text1} </p>
-                <p className={styles.text2}>{pack.description.text2}</p>
-                <p className={styles.text3}>{pack.description.text3} </p>
-              </div>
-            </div>
-          );
-        })}
+      <div
+        className={flex === '1' ? styles.cardsGrid : styles.cards}
+        onScroll={handleScroll}
+      >
+        {short
+          ? packsShort.map (pack => {
+              return (
+                <div className={styles.card} key={pack.id}>
+                  <img src={pack.imgSrc} alt={pack.imgSrc} />
+                  <div className={styles.texts}>
+                    <p className={styles.text1}>{pack.description.text1} </p>
+                    <p className={styles.text2}>{pack.description.text2}</p>
+                    <p className={styles.text3}>{pack.description.text3} </p>
+                  </div>
+                </div>
+              );
+            })
+          : packs.map (pack => {
+              return (
+                <div className={styles.card} key={pack.id}>
+                  <img src={pack.imgSrc} alt={pack.imgSrc} />
+                  <div className={styles.texts}>
+                    <p className={styles.text1}>{pack.description.text1} </p>
+                    <p className={styles.text2}>{pack.description.text2}</p>
+                    <p className={styles.text3}>{pack.description.text3} </p>
+                  </div>
+                </div>
+              );
+            })}
       </div>
       {queryTablet &&
         <ScrollBar
