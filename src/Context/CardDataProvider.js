85ead2aa2bb710b0bfc8cc66b29a7e0f22d978b1
Mauro Vera <Maurovera069@gmail.com>
2021-10-14 16:35:26 -0300

    Add one more pack

--- src/Context/CardDataProvider.js
+++ src/Context/CardDataProvider.js
@@ -1,78 +1,92 @@
-import { createContext, useState, useEffect } from 'react';
-import image1 from '../Assets/sprites/cardpack01.png';
-import image2 from '../Assets/sprites/cardpack02.png';
-import image4 from '../Assets/sprites/cardpack04.png';
+import { createContext, useState, useEffect } from "react";
+import image1 from "../Assets/sprites/cardpack01.png";
+import image2 from "../Assets/sprites/cardpack02.png";
+import image4 from "../Assets/sprites/cardpack04.png";
 
 export const CardData = createContext({});
 
-const CardDataProvider = ({children}) => {
-    const [packs, setPacks] = useState([]);
-    const [packSelected, setPackSelected] = useState({});
-    const [packToOpen, setPackToOpen] = useState({})
+const CardDataProvider = ({ children }) => {
+  const [packs, setPacks] = useState([]);
+  const [packSelected, setPackSelected] = useState({});
+  const [packToOpen, setPackToOpen] = useState({});
 
-    const setPack = (pack) => {
-        setPackSelected(pack);
-    }
+  const setPack = (pack) => {
+    setPackSelected(pack);
+  };
 
-    const setPackForOpen = (pack) => {
-        setPackToOpen(pack)
-    }
+  const setPackForOpen = (pack) => {
+    setPackToOpen(pack);
+  };
 
-    useEffect(() => {
-        //Cards hardcoded
-        setPacks(
-            [
-                {
-                    id: 1,
-                    imgSrc: image1,
-                    description : {
-                        text1: "Rare Series 1 Release",
-                        text2: "1 - DROP 1",
-                        text3: "2,00 PACK",
-                    },
-                    content: ["1 rare skin", "2 common skin"],
-                    price: 100000,
-                    stock: 300,
-                    soldOut: false,
-                    sale: true
-                },
-                {
-                    id: 2,
-                    imgSrc: image2,
-                    description : {
-                        text1: "Rare Series 1 Release",
-                        text2: "1 - DROP 1",
-                        text3: "2,00 PACK",
-                    },
-                    content: ["1 rare skin", "2 common skin"],
-                    price: 200000,
-                    stock: 255,
-                    soldOut: true,
-                    sale: false
-                },
-                {
-                    id: 4,
-                    imgSrc: image4,
-                    description : {
-                        text1: "Rare Series 1 Release",
-                        text2: "1 - DROP 1",
-                        text3: "2,00 PACK",
-                    },
-                    content: ["1 rare skin", "2 common skin"],
-                    price: 300000,
-                    stock: 450,
-                    soldOut: false,
-                    sale: false
-                },
-            ])
+  useEffect(() => {
+    //Cards hardcoded
+    setPacks([
+      {
+        id: 1,
+        imgSrc: image1,
+        description: {
+          text1: "Rare Series 1 Release",
+          text2: "1 - DROP 1",
+          text3: "2,00 PACK",
+        },
+        content: ["1 rare skin", "2 common skin"],
+        price: 100000,
+        stock: 300,
+        soldOut: false,
+        sale: true,
+      },
+      {
+        id: 2,
+        imgSrc: image2,
+        description: {
+          text1: "Rare Series 1 Release",
+          text2: "1 - DROP 1",
+          text3: "2,00 PACK",
+        },
+        content: ["1 rare skin", "2 common skin"],
+        price: 200000,
+        stock: 255,
+        soldOut: true,
+        sale: false,
+      },
+      {
+        id: 4,
+        imgSrc: image4,
+        description: {
+          text1: "Rare Series 1 Release",
+          text2: "1 - DROP 1",
+          text3: "2,00 PACK",
+        },
+        content: ["1 rare skin", "2 common skin"],
+        price: 300000,
+        stock: 450,
+        soldOut: false,
+        sale: false,
+      },
+      {
+        id: 3,
+        imgSrc: image2,
+        description: {
+          text1: "Rare Series 1 Release",
+          text2: "1 - DROP 1",
+          text3: "2,00 PACK",
+        },
+        content: ["1 rare skin", "2 common skin"],
+        price: 200000,
+        stock: 255,
+        soldOut: true,
+        sale: false,
+      },
+    ]);
+  }, []);
 
-    }, [])
-
-    return (
-        <CardData.Provider value = {{setPack, setPackForOpen, packs, packSelected, packToOpen}}>
-            {children}
-        </CardData.Provider>
-    )
-}
+  return (
+    <CardData.Provider
+      value={{ setPack, setPackForOpen, packs, packSelected, packToOpen }}
+    >
+      {children}
+    </CardData.Provider>
+  );
+};
 
 export default CardDataProvider;
