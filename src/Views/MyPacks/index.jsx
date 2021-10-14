b9c460ea44b046f67206d8388ba53f714d20c6b3
Mauro Vera <Maurovera069@gmail.com>
2021-10-14 16:37:18 -0300

    MyPacks component

--- src/Views/MyPacks/index.jsx
+++ src/Views/MyPacks/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+import Background from '../../Global-Components/Background';
+import CollectionPacks from '../Collection/Components/CollectionPacks';
+import styles from './styles.module.scss';
 
 const MyPacks = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+    const history = useHistory()
 
-export default MyPacks
+  const goBack = () => {
+    history.goBack ();
+  };
+  return (
+    <Background>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <p onClick={goBack} className={styles.back}>&#60; Go back</p>
+          <p className={styles.title}>PACK</p>
+          <div className={styles.rectangle}>
+            <CollectionPacks flex='1' />
+          </div>
+        </div>
+      </div>
+    </Background>
+  );
+};
+
+export default MyPacks;
