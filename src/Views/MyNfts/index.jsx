8554f42df70469dfe14cff1771e67bea44e17f45
Mauro Vera <Maurovera069@gmail.com>
2021-10-15 11:42:58 -0300

    MyNfts Component

--- src/Views/MyNfts/index.jsx
+++ src/Views/MyNfts/index.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+import Background from '../../Global-Components/Background';
+import CollectionNfts from '../Collection/Components/CollectionNfts';
+import styles from './styles.module.scss';
 
 const MyNfts = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const history = useHistory ();
 
-export default MyNfts
+  const goBack = () => {
+    history.goBack ();
+  };
+
+  return (
+    <Background>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <p onClick={goBack} className={styles.back}>&#60; Go back</p>
+          <p className={styles.title}>NFT</p>
+          <div className={styles.rectangle}>
+            <CollectionNfts flex='1' />
+          </div>
+        </div>
+      </div>
+    </Background>
+  );
+};
+
+export default MyNfts;
