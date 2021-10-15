4841e6abc71a78a125602a007711e1deb3d5c1b4
Mauro Vera <Maurovera069@gmail.com>
2021-10-15 11:41:46 -0300

    add grid to component

--- src/Views/Collection/Components/CollectionNfts/index.jsx
+++ src/Views/Collection/Components/CollectionNfts/index.jsx
@@ -1,10 +1,10 @@
 import React, {useContext, useState} from 'react';
 import styles from './styles.module.scss';
 import {NftData} from '../../../../Context/NftProvider';
-import { useMediaQuery } from '../../../../Hooks/useMediaQuery';
+import {useMediaQuery} from '../../../../Hooks/useMediaQuery';
 import ScrollBar from '../../../../Global-Components/ScrollBar';
 
-const CollectionNfts = () => {
+const CollectionNfts = ({flex, short}) => {
   const [scroll, setScroll] = useState ({scrollLeft: '', scrollWidth: ''});
   const queryTablet = useMediaQuery ('(max-width: 766px)');
   const {nfts} = useContext (NftData);
@@ -16,26 +16,43 @@ const CollectionNfts = () => {
     });
   };
 
+  const nftsShort = nfts.slice (0, 3);
+
   return (
     <div className={styles.cardsContainer}>
-      <div className={styles.cards} onScroll={handleScroll}>
-        {nfts.map (nft => {
-          return (
-            <div className={styles.cardNft} key={nft.id}>
-              <img className={styles.imgNft} src={nft.imgSrc} alt="nft" />
-              <div className={styles.texts}>
-                <p >Series 1 </p>
-                <p className={styles.text2}>Tron Warrior</p>
-              </div>
-            </div>
-          );
-        })}
+      <div
+        className={flex === '1' ? styles.cardsGrid : styles.cards}
+        onScroll={handleScroll}
+      >
+        {short
+          ? nftsShort.map (nft => {
+              return (
+                <div className={styles.cardNft} key={nft.id}>
+                  <img className={styles.imgNft} src={nft.imgSrc} alt="nft" />
+                  <div className={styles.texts}>
+                    <p>Series 1 </p>
+                    <p className={styles.text2}>Tron Warrior</p>
+                  </div>
+                </div>
+              );
+            })
+          : nfts.map (nft => {
+              return (
+                <div className={styles.cardNft} key={nft.id}>
+                  <img className={styles.imgNft} src={nft.imgSrc} alt="nft" />
+                  <div className={styles.texts}>
+                    <p>Series 1 </p>
+                    <p className={styles.text2}>Tron Warrior</p>
+                  </div>
+                </div>
+              );
+            })}
       </div>
       {queryTablet &&
         <ScrollBar
           width={scroll.scrollWidth}
           position={scroll.scrollLeft}
-          elements={nfts.length}
+          elements={short ? nftsShort.length : nfts.length}
         />}
     </div>
   );
