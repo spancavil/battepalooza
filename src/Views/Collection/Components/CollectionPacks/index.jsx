68a74ff7bb9528f3039eaed210cf48a91bb4e0bb
Mauro Vera <Maurovera069@gmail.com>
2021-10-15 10:56:01 -0300

    Add a version with grid

--- src/Views/Collection/Components/CollectionPacks/index.jsx
+++ src/Views/Collection/Components/CollectionPacks/index.jsx
@@ -3,11 +3,15 @@ import styles from './styles.module.scss';
 import {CardData} from '../../../../Context/CardDataProvider';
 import {useMediaQuery} from '../../../../Hooks/useMediaQuery';
 import ScrollBar from '../../../../Global-Components/ScrollBar';
+import {useHistory} from 'react-router';
+import { Link } from 'react-router-dom';
 
 const CollectionPacks = ({flex, short}) => {
   const [scroll, setScroll] = useState ({scrollLeft: '', scrollWidth: ''});
   const queryTablet = useMediaQuery ('(max-width: 766px)');
   const {packs} = useContext (CardData);
+
+  const history = useHistory ();
 
   const handleScroll = e => {
     setScroll ({
@@ -16,7 +20,11 @@ const CollectionPacks = ({flex, short}) => {
     });
   };
 
-  const packsShort = packs.slice(0, 3)
+  const onClick = id => {
+    history.push (`packs/${id}`);
+  };
+
+  const packsShort = packs.slice (0, 3);
 
   return (
     <div className={styles.cardsContainer}>
@@ -27,7 +35,7 @@ const CollectionPacks = ({flex, short}) => {
         {short
           ? packsShort.map (pack => {
               return (
-                <div className={styles.card} key={pack.id}>
+                <div onClick={() => onClick(pack.id)} className={styles.card} key={pack.id}>
                   <img src={pack.imgSrc} alt={pack.imgSrc} />
                   <div className={styles.texts}>
                     <p className={styles.text1}>{pack.description.text1} </p>
@@ -39,6 +47,7 @@ const CollectionPacks = ({flex, short}) => {
             })
           : packs.map (pack => {
               return (
+                <Link style={{textDecoration: 'none', overflow: 'visible'}} to={`/packs/${pack.id}`}>
                 <div className={styles.card} key={pack.id}>
                   <img src={pack.imgSrc} alt={pack.imgSrc} />
                   <div className={styles.texts}>
@@ -47,6 +56,7 @@ const CollectionPacks = ({flex, short}) => {
                     <p className={styles.text3}>{pack.description.text3} </p>
                   </div>
                 </div>
+                </Link>
               );
             })}
       </div>
