809cc6c4e7a717b58f863f8f6faaa098221d2d9d
sancavillwaypoint <sancavil@waypoint.la>
2021-10-18 11:54:12 -0300

    Fix .env and connection with forte

--- src/Services/auth.service.js
+++ src/Services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "https://be-battlepalooza.herokuapp.com/api/";
+const API_URL = process.env.API_URL;
 
 class AuthService {
   login(username, code, endpoint) {
