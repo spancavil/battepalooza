014434a179144b5b25e504da05cae50ae21a99d7
Mauro Vera <Maurovera069@gmail.com>
2021-10-18 11:04:18 -0300

    Create RegisterToMarketplace component

---
+++ src/Views/RegisterToMarketplace/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Background from '../../Global-Components/Background'
+
+const RegisterToMarketplace = () => {
+    return (
+        <Background>
+            
+        </Background>
+    )
+}
+
+export default RegisterToMarketplace
