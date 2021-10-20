00391cc9afe8f1246f0a41dac7a6249ff9adefae
Mauro Vera <Maurovera069@gmail.com>
2021-10-20 16:21:43 -0300

    Delete a console.log and organize the code

--- src/Views/JoinDrop/index.jsx
+++ src/Views/JoinDrop/index.jsx
@@ -1,143 +1,168 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, {useContext, useEffect, useState} from 'react';
 import Background from '../../Global-Components/Background';
 import Modal from '../../Global-Components/Modal';
 import Button from '../../Global-Components/Button';
 import styles from './styles.module.scss';
 import SubMessage from '../../Global-Components/SubMessage';
-import { createRandomNumber } from '../../Utils/createQueue';
+import {createRandomNumber} from '../../Utils/createQueue';
 import ProgressBar from './components/ProgressBar';
-import { CardData } from '../../Context/CardDataProvider';
+import {CardData} from '../../Context/CardDataProvider';
 import {useMediaQuery} from '../../Hooks/useMediaQuery';
-import { useHistory } from 'react-router';
+import {useHistory} from 'react-router';
 
 const JoinDrop = () => {
-    const {packSelected, setPackForOpen} = useContext(CardData);
-    const [number, setNumber] = useState("")
-    const [milliseconds, setMilliseconds] = useState(0);
-    const [finishCount, setFinishCount] = useState(false);
-    const [buy, setBuy] = useState(false);
-    const pack = packSelected;
+  const {packSelected, setPackForOpen} = useContext (CardData);
+  const [number, setNumber] = useState ('');
+  const [milliseconds, setMilliseconds] = useState (0);
+  const [finishCount, setFinishCount] = useState (false);
+  const [buy, setBuy] = useState (false);
+  const pack = packSelected;
 
-    const queryMobile = useMediaQuery('(max-width: 575px');
-    const history = useHistory();
+  const queryMobile = useMediaQuery ('(max-width: 575px');
+  const history = useHistory ();
 
-    const handleBuy = () => {
-        // Aqui irá la logica de cuando el usuario compre un pack
-        setBuy(true);
-    }
+  const handleBuy = () => {
+    // Aqui irá la logica de cuando el usuario compre un pack
+    setBuy (true);
+  };
 
-    const handleCount = () => {
-        setFinishCount(true);
-    }
+  const handleCount = () => {
+    setFinishCount (true);
+  };
 
-    const handleOpen = () => {
-        setPackForOpen(packSelected);
-        
-        history.push('/open-pack');
-    }
+  const handleOpen = () => {
+    setPackForOpen (packSelected);
 
-    const handleLater = () => {
-        history.push('/collection');
-    }
+    history.push ('/open-pack');
+  };
 
-    const handleClose = () => {
-        history.push(`/packs/${pack.id}`);
-    }
+  const handleLater = () => {
+    history.push ('/collection');
+  };
 
-    const handleCloseConfirmation = () => {
-        history.push('/packs');
-    }
+  const handleClose = () => {
+    history.push (`/packs/${pack.id}`);
+  };
 
-    useEffect(() => {
-        setNumber(createRandomNumber(5,15));
-    }, [])
-    
-    useEffect(()=> {
-        setMilliseconds(number*500);
-    }, [number])
+  const handleCloseConfirmation = () => {
+    history.push ('/packs');
+  };
 
-    console.log(finishCount);
-    return (
-        <Background>
-            <div className = {styles.parentContent}>
-                <div className={styles.content}>
-                    <span className={styles.line1}>
-                        {pack.description.text1} {pack.description.text2} <br/> {pack.stock} packs left <br/><br/>
-                    </span>
-                    
-                    <span className={styles.line2}>
-                        You are in Line! <br/>
-                    </span>
+  useEffect (() => {
+    setNumber (createRandomNumber (5, 15));
+  }, []);
 
-                    <span className={styles.line3}>
-                        <br/>Your number is {number} <br/><br/>
-                    </span>
-                    <span className={styles.line4}>
-                        Expected wait time {number*0.5} seconds
-                    </span>
-                    {!finishCount && <ProgressBar
-                        width={"100%"}
-                        totalTime={milliseconds}
-                        finish = {handleCount}
-                    />}
-                    {finishCount && <div style={{
-                                            width: "100%",
-                                            border: "1px solid white",
-                                            height: "33px"
-                                        }}>
-                                            <div style ={{
-                                                width: "100%",
-                                                backgroundColor: '#0146D5',
-                                                height: '33px'
-                                            }}
-                                            />
-                                        </div>}
-                </div>
-                <div className = {styles.cardImage}>
-                    <img src={pack.imgSrc} alt="pack"/>
-                </div>
+  useEffect (
+    () => {
+      setMilliseconds (number * 500);
+    },
+    [number]
+  );
+
+  return (
+    <Background>
+      <div className={styles.parentContent}>
+        <div className={styles.content}>
+          <span className={styles.line1}>
+            {pack.description.text1}
+            {' '}
+            {pack.description.text2}
+            {' '}
+            <br />
+            {' '}
+            {pack.stock}
+            {' '}
+            packs left
+            {' '}
+            <br />
+            <br />
+          </span>
+
+          <span className={styles.line2}>
+            You are in Line! <br />
+          </span>
+
+          <span className={styles.line3}>
+            <br />Your number is {number} <br /><br />
+          </span>
+          <span className={styles.line4}>
+            Expected wait time {number * 0.5} seconds
+          </span>
+          {!finishCount &&
+            <ProgressBar
+              width={'100%'}
+              totalTime={milliseconds}
+              finish={handleCount}
+            />}
+          {finishCount &&
+            <div
+              style={{
+                width: '100%',
+                border: '1px solid white',
+                height: '33px',
+              }}
+            >
+              <div
+                style={{
+                  width: '100%',
+                  backgroundColor: '#0146D5',
+                  height: '33px',
+                }}
+              />
+            </div>}
+        </div>
+        <div className={styles.cardImage}>
+          <img src={pack.imgSrc} alt="pack" />
+        </div>
+      </div>
+      {finishCount &&
+        !buy &&
+        <div className={styles.parentContainerModal}>
+          <Modal title="Checkout" handleClose={handleClose}>
+            <h3 className={styles.textDrop}>
+              Will you use
+              {pack.price}
+              nCoin to buy
+              {pack.description.text1}
+              pack?
+            </h3>
+            <Button title="BUY" width="176px" onClick={handleBuy} />
+            <SubMessage
+              text="Not enough nCoin?"
+              link="/account"
+              textLink="Charge now"
+            />
+          </Modal>
+        </div>}
+      {buy &&
+        <div className={styles.parentContainerModal}>
+          <Modal
+            title="Confirmation"
+            handleCloseConfirmation={handleCloseConfirmation}
+          >
+            <h3 className={styles.textDrop}>
+              Congratulations! Your purchase was succesful!
+              {' '}
+              <br />
+              Will you open the Pack now?
+            </h3>
+            <div className={styles.modalButtons}>
+              <Button
+                title="Open"
+                onClick={handleOpen}
+                width={queryMobile ? '126px' : '176px'}
+              />
+              <Button
+                title="Later"
+                onClick={handleLater}
+                width={queryMobile ? '126px' : '176px'}
+                style={{backgroundColor: '#0149DB'}}
+              />
             </div>
-            {(finishCount && !buy) && 
-                <div className ={styles.parentContainerModal}>
-                <Modal title = "Checkout" handleClose = {handleClose}>
-                    <h3 className={styles.textDrop}>
-                        Will you use {pack.price} nCoin to buy {pack.description.text1} pack?
-                    </h3>
-                    <Button
-                    title = "BUY"
-                    width = "176px"
-                    onClick = {handleBuy}
-                    />
-                    <SubMessage
-                    text = "Not enough nCoin?"
-                    link = "/account"
-                    textLink = "Charge now"
-                    />
-                </Modal>
-                </div>
-            }
-            {buy &&
-                <div className ={styles.parentContainerModal}>
-                <Modal title="Confirmation" handleCloseConfirmation = {handleCloseConfirmation}>
-                    <h3 className={styles.textDrop}>Congratulations! Your purchase was succesful! <br/>Will you open the Pack now?</h3>
-                    <div className={styles.modalButtons}>
-                        <Button
-                            title="Open"
-                            onClick = {handleOpen}
-                            width = {queryMobile ? "126px" : "176px"}
-                        />
-                        <Button
-                            title="Later"
-                            onClick = {handleLater}
-                            width = {queryMobile ? "126px" : "176px"}
-                            style={{backgroundColor: "#0149DB"}}
-                        />
-                    </div>
-                </Modal>
-                </div>
-            }
-        </Background>
-    )
-}
+          </Modal>
+        </div>}
+    </Background>
+  );
+};
 
 export default JoinDrop;
