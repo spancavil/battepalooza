967a3a18b276cbb5bb0251b491744758c7e4c5fa
Mauro Vera <Maurovera069@gmail.com>
2021-10-25 11:16:39 -0300

    Change a route

--- src/Views/OpenPack/index.jsx
+++ src/Views/OpenPack/index.jsx
@@ -3,49 +3,51 @@ import Background from '../../Global-Components/Background';
 import Button from '../../Global-Components/Button';
 import styles from './styles.module.scss';
 import {CardData} from '../../Context/CardDataProvider';
-import {Redirect} from 'react-router';
+import {Redirect, useHistory} from 'react-router';
 import CardAnimation from '../CardAnimation';
 
 const OpenPack = () => {
   const [flow, setFlow] = useState (1);
   const {packToOpen} = useContext (CardData);
 
+  const history = useHistory ();
+
   const nextFlow = () => {
     setFlow (flow + 1);
-  }
+  };
+
+  const openLater = () => {
+    history.push ('/collection/packs');
+  };
 
   const timerFlow = () => {
-    setTimeout(()=> {
+    setTimeout (() => {
       setFlow (flow + 1);
-    }, 3000)
-  }
+    }, 3000);
+  };
 
   return !packToOpen.imgSrc
     ? <Redirect to="/" />
     : <Background>
-      {flow === 1 &&
-        <div className={styles.container}>
-          <div className={styles.deg}>
-            <div className={styles.card}>
-              <img src={packToOpen.imgSrc} alt="pack" />
-              <div className={styles.down}>
-                <Button title="OPEN" onClick = {nextFlow}/>
-                <p>OPEN LATER</p>
+        {flow === 1 &&
+          <div className={styles.container}>
+            <div className={styles.deg}>
+              <div className={styles.card}>
+                <img src={packToOpen.imgSrc} alt="pack" />
+                <div className={styles.down}>
+                  <Button title="OPEN" onClick={nextFlow} />
+                  <p onClick={openLater}>OPEN LATER</p>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      }
-      {flow === 2 &&
-        <div className = {styles.container2}>
-          <h2>Video "open pack" playing...</h2>
-          {timerFlow()}
-        </div>
-      }
-      {flow === 3 &&
-        <CardAnimation/>
-      }
-    </Background>;
+          </div>}
+        {flow === 2 &&
+          <div className={styles.container2}>
+            <h2>Video "open pack" playing...</h2>
+            {timerFlow ()}
+          </div>}
+        {flow === 3 && <CardAnimation />}
+      </Background>;
 };
 
 export default OpenPack;
