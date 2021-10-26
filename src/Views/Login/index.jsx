f237826a64dedb83b1e1ab4346809826451cd8a1
Mauro Vera <Maurovera069@gmail.com>
2021-10-26 11:18:56 -0300

    You can now submit with the enter

--- src/Views/Login/index.jsx
+++ src/Views/Login/index.jsx
@@ -1,6 +1,6 @@
 import React, {useContext, useState} from 'react';
 import {useHistory} from 'react-router';
-import { UserData } from '../../Context/UserProvider';
+import {UserData} from '../../Context/UserProvider';
 import Button from '../../Global-Components/Button';
 import Input from '../../Global-Components/Input';
 import Modal from '../../Global-Components/Modal';
@@ -8,12 +8,11 @@ import SubMessage from '../../Global-Components/SubMessage';
 import authService from '../../Services/auth.service';
 import styles from './styles.module.scss';
 
-
 const Login = () => {
   const [email, setEMail] = useState ('Debe ser un email válidoc');
   const [errorEmail, setErrorEmail] = useState ('');
 
-  const {setMail} = useContext(UserData);
+  const {setMail} = useContext (UserData);
 
   const history = useHistory ();
 
@@ -22,24 +21,27 @@ const Login = () => {
   };
 
   const handleClose = () => {
-    history.push('/')
-  }
+    history.push ('/');
+  };
 
-  const onLogin = async () => {
-    if(!(/\S+@\S+\.\S+/.test(email))){
-      setErrorEmail ('Input a valid email')
+  const onLogin = async e => {
+    e.preventDefault ();
+
+    if (!/\S+@\S+\.\S+/.test (email)) {
+      setErrorEmail ('Input a valid email');
     } else {
       setErrorEmail ('');
-      setMail(email);
-      const response = await authService.getVerificationCode(email);
+      setMail (email);
+      const response = await authService.getVerificationCode (email);
 
-      console.log(response)
+      console.log (response);
+      console.log ('Me active');
 
-      if (response.success === false){
-        alert(response.message)
+      if (response.success === false) {
+        alert (response.message);
       } else {
-        alert(response.message)
-        history.push('/verification')
+        alert (response.message);
+        history.push ('/verification');
       }
     }
   };
@@ -47,18 +49,28 @@ const Login = () => {
   return (
     <div className={styles.container}>
       <Modal title="LOGIN" handleClose={handleClose}>
-        <div className={styles.inputContainer}>
-          <Input
-            label="Email"
-            width="100%"
-            type="email"
-            handleChange={email => changeEmail (email)}
-          />
-          {errorEmail && <span className={styles.errorMessage}>{errorEmail}</span>}
-        </div>
-        <div style={{paddingTop: '25px'}}>
-          <Button title="GET CODE" onClick={onLogin} />
-        </div>
+        <form onSubmit={onLogin}>
+          <div className={styles.inputContainer}>
+            <Input
+              label="Email"
+              width="100%"
+              type="email"
+              handleChange={email => changeEmail (email)}
+            />
+            {errorEmail &&
+              <span className={styles.errorMessage}>{errorEmail}</span>}
+          </div>
+          <div
+            style={{
+              paddingTop: '25px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Button title="GET CODE" />
+          </div>
+        </form>
         <SubMessage
           text="Do not have an account?"
           link="/signup"
--- src/Views/Verification/index.jsx
+++ src/Views/Verification/index.jsx
@@ -1,87 +1,99 @@
-import React, { useContext } from 'react';
+import React, {useContext} from 'react';
 import Button from '../../Global-Components/Button';
 import Input from './Components/Input';
 import Modal from '../../Global-Components/Modal';
 import styles from './styles.module.scss';
-import { useState } from 'react';
-import { UserData } from '../../Context/UserProvider';
+import {useState} from 'react';
+import {UserData} from '../../Context/UserProvider';
 import authService from '../../Services/auth.service';
-import { useHistory } from 'react-router';
+import {useHistory} from 'react-router';
 
 const Verification = () => {
-  const [code, setCode] = useState("");
-  const [errorCode, setErrorCode] = useState("")
-  const { setCodeVerification, setTheToken, email, navigation, firstLogin } = useContext(UserData);
-  const history = useHistory();
+  const [code, setCode] = useState ('');
+  const [errorCode, setErrorCode] = useState ('');
+  const {
+    setCodeVerification,
+    setTheToken,
+    email,
+    navigation,
+    firstLogin,
+  } = useContext (UserData);
+  const history = useHistory ();
 
-  const handleChange = (codigo) => {
-    console.log(codigo.length);
-    if (codigo.length === 7 ){
-      const codigoAux = codigo.replace(/\s+/g , '');
-      console.log(codigoAux);
-      setCode(codigoAux);
+  const handleChange = codigo => {
+    console.log (codigo.length);
+    if (codigo.length === 7) {
+      const codigoAux = codigo.replace (/\s+/g, '');
+      console.log (codigoAux);
+      setCode (codigoAux);
     } else {
-      setCode(codigo)
-    };
-  }
+      setCode (codigo);
+    }
+  };
 
   const handleClose = () => {
-    history.push('/')
-  }
+    history.push ('/');
+  };
 
   const handleSignOut = () => {
-    setTimeout(()=> {
+    setTimeout (() => {
       localStorage.removeItem ('user');
       history.push ('/');
-      alert("Session expired. Please login again.")
+      alert ('Session expired. Please login again.');
       window.location.reload ();
-    }, 15 * 60 * 1000)
-  }
+    }, 15 * 60 * 1000);
+  };
 
-
-  const submitCode = async () => {
-    if (!(/^\d{6}$/.test(code))) {
-      setErrorCode("Input a valid code")
+  const submitCode = async e => {
+    e.preventDefault ();
+    if (!/^\d{6}$/.test (code)) {
+      setErrorCode ('Input a valid code');
     } else {
-      setErrorCode('');
-      setCodeVerification(code)
+      setErrorCode ('');
+      setCodeVerification (code);
 
-      const response = firstLogin ? 
-      await authService.login(email, code, "/first-login"):
-      await authService.login(email, code, "" );
+      const response = firstLogin
+        ? await authService.login (email, code, '/first-login')
+        : await authService.login (email, code, '');
 
       //console.log(response);
 
       if (response.data.message) {
-        alert(response.data.message)
+        alert (response.data.message);
       } else {
-        setTheToken(response);
-        handleSignOut();
-        history.push(navigation);
+        setTheToken (response);
+        handleSignOut ();
+        history.push (navigation);
       }
-
     }
-
-  }
+  };
 
   return (
     <div className={styles.container}>
       <Modal title="VERIFICATION" handleClose={handleClose}>
-        <div className={styles.inputContainer}>
-          <Input
-            label="Code"
-            width="100%"
-            subtitle="Input the 6 digit code that has been sent to your email"
-            handleChange={(code) => handleChange(code)}
-            value = {code}
-          />
-          {errorCode && <span className={styles.errorMessage}>{errorCode}</span>}
-        </div>
-        <div style={{ paddingTop: '40px' }}>
-          <Button title="VERIFY"
-            onClick={submitCode}
-          />
-        </div>
+        <form onSubmit={submitCode}>
+          <div className={styles.inputContainer}>
+            <Input
+              label="Code"
+              width="100%"
+              subtitle="Input the 6 digit code that has been sent to your email"
+              handleChange={code => handleChange (code)}
+              value={code}
+            />
+            {errorCode &&
+              <span className={styles.errorMessage}>{errorCode}</span>}
+          </div>
+          <div
+            style={{
+              paddingTop: '25px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Button title="VERIFY" />
+          </div>
+        </form>
         <span className={styles.message}>
           Did not receive email? Check your spam folder. <br />
           If you have not received your email contact support
