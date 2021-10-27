79be1c2966a6ec451b8a95fdef96b6816d4fc728
Mauro Vera <Maurovera069@gmail.com>
2021-10-27 16:17:38 -0300

    Modal Open / Later

--- src/Views/Collection/Components/CollectionPacks/index.jsx
+++ src/Views/Collection/Components/CollectionPacks/index.jsx
@@ -4,10 +4,16 @@ import {CardData} from '../../../../Context/CardDataProvider';
 import {useMediaQuery} from '../../../../Hooks/useMediaQuery';
 import ScrollBar from '../../../../Global-Components/ScrollBar';
 import {Link} from 'react-router-dom';
+import ModalII from '../../../JoinDrop/components/Modal';
+import Button from '../../../../Global-Components/Button';
 
 const CollectionPacks = () => {
   const [scroll, setScroll] = useState ({scrollLeft: '', scrollWidth: ''});
+  const [modal, setModal] = useState (false);
+
   const queryTablet = useMediaQuery ('(max-width: 766px)');
+  const queryMobile = useMediaQuery ('(max-width: 575px');
+
   const {packs} = useContext (CardData);
 
   const handleScroll = e => {
@@ -22,21 +28,39 @@ const CollectionPacks = () => {
       <div className={styles.cardsGrid} onScroll={handleScroll}>
         {packs.map (pack => {
           return (
-            <Link
-              style={{textDecoration: 'none', overflow: 'visible'}}
-              to={`/packs/${pack.id}`}
+            <div
+              onClick={() => setModal (true)}
+              className={styles.card}
+              key={pack.id}
             >
-              <div className={styles.card} key={pack.id}>
-                <img src={pack.imgSrc} alt={pack.imgSrc} />
-                <div className={styles.texts}>
-                  <p className={styles.text1}>{pack.description.text1} </p>
-                  <p className={styles.text2}>{pack.description.text2}</p>
-                  <p className={styles.text3}>{pack.description.text3} </p>
-                </div>
+              <img src={pack.imgSrc} alt={pack.imgSrc} />
+              <div className={styles.texts}>
+                <p className={styles.text1}>{pack.description.text1} </p>
+                <p className={styles.text2}>{pack.description.text2}</p>
+                <p className={styles.text3}>{pack.description.text3} </p>
               </div>
-            </Link>
+            </div>
           );
         })}
+        {modal &&
+          <div className={styles.modalContainer}>
+            <ModalII title="Open Pack">
+              <div className={styles.modalButtons}>
+                <Link to="/open-pack">
+                  <Button
+                    title="Open"
+                    width={queryMobile ? '126px' : '176px'}
+                  />
+                </Link>
+                <Button
+                  title="Later"
+                  onClick={() => setModal (false)}
+                  width={queryMobile ? '126px' : '176px'}
+                  style={{backgroundColor: '#0149DB'}}
+                />
+              </div>
+            </ModalII>
+          </div>}
       </div>
       {queryTablet &&
         <ScrollBar
