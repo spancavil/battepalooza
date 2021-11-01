01dc0a3145236741345a57fcd1c861b43a00abc6
Mauro Vera <Maurovera069@gmail.com>
2021-11-01 15:10:47 -0300

    Add PopUp

--- src/Views/Login/index.jsx
+++ src/Views/Login/index.jsx
@@ -1,9 +1,11 @@
 import React, {useContext, useState} from 'react';
+import {Fragment} from 'react';
 import {useHistory} from 'react-router';
 import {UserData} from '../../Context/UserProvider';
 import Button from '../../Global-Components/Button';
 import Input from '../../Global-Components/Input';
 import Modal from '../../Global-Components/Modal';
+import PopUp from '../../Global-Components/PopUp';
 import SubMessage from '../../Global-Components/SubMessage';
 import authService from '../../Services/auth.service';
 import styles from './styles.module.scss';
@@ -11,6 +13,8 @@ import styles from './styles.module.scss';
 const Login = () => {
   const [email, setEMail] = useState ('Debe ser un email válidoc');
   const [errorEmail, setErrorEmail] = useState ('');
+  const [popUp, setPopUp] = useState (false);
+  const [message, setMessage] = useState ('');
 
   const {setMail} = useContext (UserData);
 
@@ -34,51 +38,57 @@ const Login = () => {
       setMail (email);
       const response = await authService.getVerificationCode (email);
 
-      console.log (response);
-      console.log ('Me active');
-
       if (response.success === false) {
-        alert (response.message);
+        setMessage (response.message);
+        setPopUp (true);
+        setTimeout (() => setPopUp (false), 1500);
       } else {
-        alert (response.message);
-        history.push ('/verification');
+        setMessage (response.message);
+        setPopUp (true);
+        setTimeout (() => setPopUp (false), 1500);
+        setTimeout (() => history.push ('/verification'), 2000);
       }
     }
   };
 
   return (
-    <div className={styles.container}>
-      <Modal title="LOGIN" handleClose={handleClose}>
-        <form style={{width: '100%'}} onSubmit={onLogin}>
-          <div className={styles.inputContainer}>
-            <Input
-              label="Email"
-              width="100%"
-              type="email"
-              handleChange={email => changeEmail (email)}
-              autofocus
-            />
-            {errorEmail &&
-              <span className={styles.errorMessage}>{errorEmail}</span>}
-          </div>
-          <div
-            style={{
-              paddingTop: '25px',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <Button title="GET CODE" />
-          </div>
-        </form>
-        <SubMessage
-          text="Do not have an account?"
-          link="/signup"
-          textLink="Sign up"
-        />
-      </Modal>
-    </div>
+    <Fragment>
+      <div className={styles.container}>
+        <Modal title="LOGIN" handleClose={handleClose}>
+          <form style={{width: '100%'}} onSubmit={onLogin}>
+            <div className={styles.inputContainer}>
+              <Input
+                label="Email"
+                width="100%"
+                type="email"
+                handleChange={email => changeEmail (email)}
+                autofocus
+              />
+              {errorEmail &&
+                <span className={styles.errorMessage}>{errorEmail}</span>}
+            </div>
+            <div
+              style={{
+                paddingTop: '25px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Button title="GET CODE" />
+            </div>
+          </form>
+          <SubMessage
+            text="Do not have an account?"
+            link="/signup"
+            textLink="Sign up"
+          />
+        </Modal>
+      </div>
+        <div className={styles.popUpContainer}>
+          <PopUp text={message || 'Algo ocurrio'} />
+        </div>
+    </Fragment>
   );
 };
 
