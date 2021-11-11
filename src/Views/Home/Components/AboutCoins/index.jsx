05d1d5fddbb5c5e42104f782dcc2fcbfaa1e0ddf
Mauro Vera <Maurovera069@gmail.com>
2021-11-11 10:08:58 -0300

    Fix banner coins

--- src/Views/Home/Components/AboutCoins/index.jsx
+++ src/Views/Home/Components/AboutCoins/index.jsx
@@ -5,12 +5,11 @@ const AboutCoins = () => {
   return (
     <div className={styles.content2}>
       <article>
-        <h2> Play to earn<b>!</b></h2>
-        <h4><b>NCoin</b> is a currency in Battlepalooza.</h4>
+        <h2> Earn more <br /> with Battlepalooza<b>!</b></h2>
+        <h4>Trade in <b>NCoins</b> for incredible prizes</h4>
         <p>
-          Players can buy NCoins and get incredible prizes or get them by playing the game!
-          <br />
-          We will release more information regarding NCoin in the future.
+          The more you play, the more NCoins you
+          earn! <br /> NCoins are also available for purchase
         </p>
       </article>
     </div>
