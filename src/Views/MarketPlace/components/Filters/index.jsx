300a9a9b9606c1603742c1ccffb4dd2d43731d24
Mauro Vera <Maurovera069@gmail.com>
2021-11-30 17:19:55 -0300

    Filter Responsive

--- src/Views/MarketPlace/components/Filters/index.jsx
+++ src/Views/MarketPlace/components/Filters/index.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {useMediaQuery} from '../../../../Hooks/useMediaQuery';
 
 import styles from './styles.module.scss';
 
@@ -9,61 +10,187 @@ const Filters = () => {
     EPIC: false,
     LEGENDARY: false,
   });
+  const breakpoint = useMediaQuery ('(max-width: 1199px)');
+  const [menu, setMenu] = useState (false);
 
   const onClick = e => {
     setFilters ({...filters, [e.target.name]: e.target.checked});
   };
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.title}>
-        <h2>
-          FILTERS
-        </h2>
+  return breakpoint
+    ? <>
+    <div onClick={() => setMenu (!menu)} className={styles.btnFilter}>
+        <svg
+          width="28"
+          height="28"
+          viewBox="0 0 28 28"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M4.91016 14.5105V2.04926"
+            stroke="white"
+            stroke-width="2.8"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M4.91016 25.8692V18.7641"
+            stroke="white"
+            stroke-width="2.8"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M23 16.3413V2.04926"
+            stroke="white"
+            stroke-width="2.8"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M23 25.8692V20.5744"
+            stroke="white"
+            stroke-width="2.8"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M13.9551 4.43125V2.04926"
+            stroke="white"
+            stroke-width="4"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M13.9551 25.8693V8.86179"
+            stroke="white"
+            stroke-width="2.8"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M2.44727 14.5105H7.0207"
+            stroke="white"
+            stroke-width="3.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M20.7129 16.3548H25.2863"
+            stroke="white"
+            stroke-width="3.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M11.668 8.27646H16.2414"
+            stroke="white"
+            stroke-width="3.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
       </div>
-      <div className={styles.filters}>
-        <label className={styles.filter}>
-          <input
-            onClick={e => onClick (e)}
-            value={filters.COMMON}
-            type="checkbox"
-            name="COMMON"
-          />
-          COMMON
-        </label>
+      {menu &&
+        breakpoint &&
+        <div className={styles.menuFilter}>
+          <h2>
+            FILTERS
+          </h2>
+          <div className={styles.filtersMobile}>
+            <label className={styles.filter}>
+              <input
+                onClick={e => onClick (e)}
+                value={filters.COMMON}
+                type="checkbox"
+                name="COMMON"
+              />
+              COMMON
+            </label>
 
-        <label className={styles.filter}>
-          <input
-            onClick={e => onClick (e)}
-            value={filters.RARE}
-            type="checkbox"
-            name="RARE"
-          />
-          RARE
-        </label>
+            <label className={styles.filter}>
+              <input
+                onClick={e => onClick (e)}
+                value={filters.RARE}
+                type="checkbox"
+                name="RARE"
+              />
+              RARE
+            </label>
 
-        <label className={styles.filter}>
-          <input
-            onClick={e => onClick (e)}
-            value={filters.LEGENDARY}
-            type="checkbox"
-            name="EPIC"
-          />
-          EPIC
-        </label>
+            <label className={styles.filter}>
+              <input
+                onClick={e => onClick (e)}
+                value={filters.LEGENDARY}
+                type="checkbox"
+                name="EPIC"
+              />
+              EPIC
+            </label>
 
-        <label className={styles.filter}>
-          <input
-            onClick={e => onClick (e)}
-            value={filters.LEGENDARY}
-            type="checkbox"
-            name="LEGENDARY"
-          />
-          LEGENDARY
-        </label>
-      </div>
-    </div>
-  );
+            <label className={styles.filter}>
+              <input
+                onClick={e => onClick (e)}
+                value={filters.LEGENDARY}
+                type="checkbox"
+                name="LEGENDARY"
+              />
+              LEGENDARY
+            </label>
+          </div>
+        </div>}
+        {menu &&
+        breakpoint && <div onClick={() => setMenu(false)} className={styles.bg}></div> }
+</>
+    : <div className={styles.container}>
+        <div className={styles.title}>
+          <h2>
+            FILTERS
+          </h2>
+        </div>
+        <div className={styles.filters}>
+          <label className={styles.filter}>
+            <input
+              onClick={e => onClick (e)}
+              value={filters.COMMON}
+              type="checkbox"
+              name="COMMON"
+            />
+            COMMON
+          </label>
+
+          <label className={styles.filter}>
+            <input
+              onClick={e => onClick (e)}
+              value={filters.RARE}
+              type="checkbox"
+              name="RARE"
+            />
+            RARE
+          </label>
+
+          <label className={styles.filter}>
+            <input
+              onClick={e => onClick (e)}
+              value={filters.LEGENDARY}
+              type="checkbox"
+              name="EPIC"
+            />
+            EPIC
+          </label>
+
+          <label className={styles.filter}>
+            <input
+              onClick={e => onClick (e)}
+              value={filters.LEGENDARY}
+              type="checkbox"
+              name="LEGENDARY"
+            />
+            LEGENDARY
+          </label>
+        </div>
+      </div>;
 };
 
 export default Filters;
