3d10fbc4a67d690e453a411c79ec555ca0c012a3
Mauro Vera <Maurovera069@gmail.com>
2021-11-30 14:38:44 -0300

    Filter states

--- src/Views/MarketPlace/components/Filters/index.jsx
+++ src/Views/MarketPlace/components/Filters/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import {filters} from './Filters';
+import React, {useState} from 'react';
 
 import styles from './styles.module.scss';
 
 const Filters = () => {
+  const [filters, setFilters] = useState ({
+    COMMON: false,
+    RARE: false,
+    EPIC: false,
+    LEGENDARY: false,
+  });
+
+  const onClick = e => {
+    setFilters ({...filters, [e.target.name]: e.target.checked});
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>
@@ -12,12 +22,45 @@ const Filters = () => {
         </h2>
       </div>
       <div className={styles.filters}>
-        {filters.map ((filter, idx) => (
-          <div className={styles.filter} key={idx}>
-            <input type="checkbox" />
-            <p>{filter}</p>
-          </div>
-        ))}
+        <label className={styles.filter}>
+          <input
+            onClick={e => onClick (e)}
+            value={filters.COMMON}
+            type="checkbox"
+            name="COMMON"
+          />
+          COMMON
+        </label>
+
+        <label className={styles.filter}>
+          <input
+            onClick={e => onClick (e)}
+            value={filters.RARE}
+            type="checkbox"
+            name="RARE"
+          />
+          RARE
+        </label>
+
+        <label className={styles.filter}>
+          <input
+            onClick={e => onClick (e)}
+            value={filters.LEGENDARY}
+            type="checkbox"
+            name="EPIC"
+          />
+          EPIC
+        </label>
+
+        <label className={styles.filter}>
+          <input
+            onClick={e => onClick (e)}
+            value={filters.LEGENDARY}
+            type="checkbox"
+            name="LEGENDARY"
+          />
+          LEGENDARY
+        </label>
       </div>
     </div>
   );
