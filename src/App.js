6bcb6051c434b3d112330e8839e16fa5cf688310
spancavil <soancavil@gmail.com>
2021-12-06 17:41:01 -0300

    Account protected view

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import HomeContainer from "./Views/Home";
 import NavBar from "./Views/Navbar";
 import NotFound from "./Views/NotFound";
@@ -8,16 +8,16 @@ import Login from "./Views/Login";
 import Verification from "./Views/Verification";
 import NeedLogin from "./Views/NeedLogin";
 import "./Styles/Global.scss";
-import UserProvider from "./Context/UserProvider";
-import Packs from "./Views/Packs";
-import PackDetail from "./Views/PackDetail";
+import UserProvider, { UserData } from "./Context/UserProvider";
+// import Packs from "./Views/Packs";
+// import PackDetail from "./Views/PackDetail";
 import CardDataProvider from "./Context/CardDataProvider";
-import JoinDrop from "./Views/JoinDrop";
-import OpenPack from "./Views/OpenPack";
+// import JoinDrop from "./Views/JoinDrop";
+// import OpenPack from "./Views/OpenPack";
 //import Collection from "./Views/Collection";
 // import MyPacks from "./Views/MyPacks";
 // import MyNfts from "./Views/MyNfts";
-import CardAnimation from "./Views/CardAnimation";
+// import CardAnimation from "./Views/CardAnimation";
 import NftProvider from "./Context/NftProvider";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import ToMarketplace from "./Views/ToMarketplace";
@@ -25,23 +25,27 @@ import Privacy from "./Views/PrivacyAndTerms/Privacy";
 import TermsOfUse from "./Views/PrivacyAndTerms/TermsOfUse";
 import MarketPlace from "./Views/MarketPlace";
 import MarketplaceDetail from "./Views/MarketplaceDetail";
+import { useContext } from "react";
 
 function App() {
+
+  const {userData} = useContext(UserData);
+
   return (
-    <BrowserRouter>
-      <UserProvider>
-        <NftProvider>
-          <GoogleReCaptchaProvider
-            reCaptchaKey={process.env.REACT_APP_RECAPTCHA_KEY}
-            language="en"
-            scriptProps={{
-              async: false, // optional, default to false,
-              defer: false, // optional, default to false
-              appendTo: "head", // optional, default to "head", can be "head" or "body",
-              nonce: undefined, // optional, default undefined
-            }}
+    <UserProvider>
+      <NftProvider>
+        <GoogleReCaptchaProvider
+          reCaptchaKey={process.env.REACT_APP_RECAPTCHA_KEY}
+          language="en"
+          scriptProps={{
+            async: false, // optional, default to false,
+            defer: false, // optional, default to false
+            appendTo: "head", // optional, default to "head", can be "head" or "body",
+            nonce: undefined, // optional, default undefined
+          }}
           >
-            <CardDataProvider>
+          <CardDataProvider>
+            <BrowserRouter>
               <NavBar />
               <Switch>
                 <Route exact path="/" component={HomeContainer} />
@@ -49,15 +53,18 @@ function App() {
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/verification" component={Verification} />
                 <Route exact path="/needlogin" component={NeedLogin} />
-                <Route exact path="/account" component={Account} />
-                <Route exact path="/packs" component={Packs} />
+
+                <Route exact path="/account">
+                  {userData ? <Account/> : <Redirect to="/needlogin" />}
+                </Route>
+                {/* <Route exact path="/packs" component={Packs} />
                 <Route exact path="/packs/:id" component={PackDetail} />
                 <Route exact path="/join-drop" component={JoinDrop} />
-                <Route exact path="/open-pack" component={OpenPack} />
+                <Route exact path="/open-pack" component={OpenPack} /> */}
                 {/* <Route exact path="/collection" component={Collection} /> */}
                 {/* <Route exact path="/my-packs" component={MyPacks} />
                 <Route exact path="/my-skins" component={MyNfts} /> */}
-                <Route exact path="/card-animation" component={CardAnimation} />
+                {/* <Route exact path="/card-animation" component={CardAnimation} /> */}
                 <Route
                   exact
                   path="/to-marketplace/:id"
@@ -69,11 +76,11 @@ function App() {
                 <Route exact path="/marketplace/:nftId" component={MarketplaceDetail}/>
                 <Route path="" component={NotFound} />
               </Switch>
-            </CardDataProvider>
-          </GoogleReCaptchaProvider>
-        </NftProvider>
-      </UserProvider>
-    </BrowserRouter>
+            </BrowserRouter>
+          </CardDataProvider>
+        </GoogleReCaptchaProvider>
+      </NftProvider>
+    </UserProvider>
   );
 }
 
