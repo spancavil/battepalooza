2d275985cbcc37d9907e2b2e716bb95b71e2d2ea
Mauro Vera <Maurovera069@gmail.com>
2021-12-10 13:20:11 -0300

    Fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import HomeContainer from "./Views/Home";
-import NavBar from "./Views/Navbar";
-import NotFound from "./Views/NotFound";
-import Account from "./Views/Account";
-import SignUp from "./Views/SignUp";
-import Login from "./Views/Login";
-import Verification from "./Views/Verification";
-import NeedLogin from "./Views/NeedLogin";
-import "./Styles/Global.scss";
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import HomeContainer from './Views/Home';
+import NavBar from './Views/Navbar';
+import NotFound from './Views/NotFound';
+import Account from './Views/Account';
+import SignUp from './Views/SignUp';
+import Login from './Views/Login';
+import Verification from './Views/Verification';
+import NeedLogin from './Views/NeedLogin';
+import './Styles/Global.scss';
 // import Packs from "./Views/Packs";
 // import PackDetail from "./Views/PackDetail";
 // import JoinDrop from "./Views/JoinDrop";
@@ -16,17 +16,16 @@ import "./Styles/Global.scss";
 // import MyPacks from "./Views/MyPacks";
 // import MyNfts from "./Views/MyNfts";
 // import CardAnimation from "./Views/CardAnimation";
-import ToMarketplace from "./Views/ToMarketplace";
-import Privacy from "./Views/PrivacyAndTerms/Privacy";
-import TermsOfUse from "./Views/PrivacyAndTerms/TermsOfUse";
-import MarketPlace from "./Views/MarketPlace";
-import MarketplaceDetail from "./Views/MarketplaceDetail";
-import { useContext } from "react";
-import { UserData } from "./Context/UserProvider";
+import ToMarketplace from './Views/ToMarketplace';
+import Privacy from './Views/PrivacyAndTerms/Privacy';
+import TermsOfUse from './Views/PrivacyAndTerms/TermsOfUse';
+import MarketPlace from './Views/MarketPlace';
+import MarketplaceDetail from './Views/MarketplaceDetail';
+import {useContext} from 'react';
+import {UserData} from './Context/UserProvider';
 
-function App() {
-
-  const {userData} = useContext(UserData);
+function App () {
+  const {userData} = useContext (UserData);
 
   return (
     <BrowserRouter>
@@ -39,7 +38,7 @@ function App() {
         <Route exact path="/needlogin" component={NeedLogin} />
 
         <Route exact path="/account">
-          { Object.keys(userData).length === 0 ? <NeedLogin/> : <Account/> }
+          {Object.keys (userData).length === 0 ? <NeedLogin /> : <Account />}
         </Route>
         {/* <Route exact path="/packs" component={Packs} />
         <Route exact path="/packs/:id" component={PackDetail} />
@@ -49,15 +48,11 @@ function App() {
         {/* <Route exact path="/my-packs" component={MyPacks} />
         <Route exact path="/my-skins" component={MyNfts} /> */}
         {/* <Route exact path="/card-animation" component={CardAnimation} /> */}
-        <Route
-          exact
-          path="/to-marketplace/:id"
-          component={ToMarketplace}
-        />
-        <Route exact path="/privacy-policy" component={Privacy}></Route>
-        <Route exact path="/terms-of-use" component={TermsOfUse}></Route>
-        <Route exact path = "/marketplace" component={MarketPlace}></Route>
-        <Route exact path="/marketplace/:nftId" component={MarketplaceDetail}/>
+        <Route exact path="/to-marketplace/:id" component={ToMarketplace} />
+        <Route exact path="/privacy-policy" component={Privacy} />
+        <Route exact path="/terms-of-use" component={TermsOfUse} />
+        <Route exact path="/marketplace" component={MarketPlace} />
+        <Route exact path="/marketplace/:nftId" component={MarketplaceDetail} />
         <Route path="" component={NotFound} />
       </Switch>
     </BrowserRouter>
