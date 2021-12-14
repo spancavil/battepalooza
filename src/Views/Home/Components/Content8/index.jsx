25903d49972e782e2168d4ebb081b1fe3c03281a
spancavil <soancavil@gmail.com>
2021-12-14 15:58:58 -0300

    parallax example

--- src/Views/Home/Components/Content8/index.jsx
+++ src/Views/Home/Components/Content8/index.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './styles.module.scss';
 import img5 from '../../../../Assets/img/img5.png';
 
 const Content8 = () => {
+  useEffect(() => {
+    
+    const text5 = document.getElementById("text5");
+    const imagen = document.getElementById("img5");
+
+    const parallax = () => {
+      let scrollY = window.scrollY;
+      if (scrollY > 3208){
+        imagen.style.top = (scrollY - 3508) * 0.15 + 'px';
+        text5.style.top = (scrollY - 3390) * 0.30 + 'px';
+        console.log(scrollY);
+      }
+    }
+
+    window.addEventListener('scroll', parallax)
+
+    return () => {
+      window.removeEventListener('scroll', parallax);
+      console.log("No más parallax");
+    }
+  }, [])
+
   return (
     <div className={styles.content8}>
       <article>
         <section>
-          <div>
+          <div
+          id = "img5"
+          >
             <img
-              src={img5}
-              alt="BattlePalooza"
+            src={img5}
+            alt="BattlePalooza"
             />
           </div>
-          <div>
+          <div
+          id = "text5"
+          >
             <h3>Out-of-this-World Fun for Everyone </h3>
             <p>
               Battlepalooza is designed for players of all skill levels. You don’t have a to be a hardcore gamer to stand a chance. The game features a top down view for easier navigation. The combat system rewards players with a blend of strategy, skill and luck -- and not just reaction time.
