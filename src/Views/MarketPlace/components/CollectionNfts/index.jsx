5987465f7e3ccc00d7bf00a2475ba69d121364bf
Mauro Vera <Maurovera069@gmail.com>
2021-12-15 15:23:51 -0300

    Filters SIN TERMINAR

--- src/Views/MarketPlace/components/CollectionNfts/index.jsx
+++ src/Views/MarketPlace/components/CollectionNfts/index.jsx
@@ -5,26 +5,57 @@ import {Link} from 'react-router-dom';
 import {useMediaQuery} from '../../../../Hooks/useMediaQuery';
 import Pagination from '../Pagination';
 
-const CollectionNfts = () => {
+const CollectionNfts = ({filters}) => {
   const [page, setPage] = useState (1);
-  const [xPage, setxPage] = useState (10);
+  const [xPage, setxPage] = useState (25);
   const {nfts} = useContext (NftData);
+  const [nftsFiltered, setNftFiltered] = useState (nfts);
 
   const breakpoint = useMediaQuery ('(max-width: 1200px)');
 
   useEffect (
     () => {
-      breakpoint ? setxPage (4) : setxPage (10);
+      breakpoint ? setxPage (4) : setxPage (25);
     },
     [breakpoint]
   );
 
-  const max = nfts.length / xPage;
+  useEffect (
+    () => {
+      setNftFiltered (nfts);
+    },
+    [nfts]
+  );
+
+  console.log (nftsFiltered);
+
+  useEffect (
+    () => {
+      if (filters.COMMON)
+        setNftFiltered (nftsFiltered.filter (nft => nft.rare === 'COMMON'));
+      if (filters.RARE)
+        setNftFiltered (nftsFiltered.filter (nft => nft.rare === 'RARE'));
+      if (filters.EPIC)
+        setNftFiltered (nftsFiltered.filter (nft => nft.rare === 'EPIC'));
+      if (filters.LEGENDARY)
+        setNftFiltered (nftsFiltered.filter (nft => nft.rare === 'LEGENDARY'));
+      if (
+        !filters.COMMON &&
+        !filters.RARE &&
+        !filters.EPIC &&
+        !filters.LEGENDARY
+      )
+        setNftFiltered (nfts);
+    },
+    [filters, nfts, nftsFiltered]
+  );
+
+  const max = nftsFiltered.length / xPage;
 
   return (
     <div className={styles.cardsContainer}>
       <div className={styles.cards}>
-        {nfts
+        {nftsFiltered
           .slice ((page - 1) * xPage, (page - 1) * xPage + xPage)
           .map (nft => {
             return (
