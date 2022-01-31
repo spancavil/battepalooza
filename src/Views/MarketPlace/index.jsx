7a3632e0adea73c77930bbcce2bb84604a19edf2
spancavil <soancavil@gmail.com>
2022-01-31 11:35:39 -0300

    Error handling

--- src/Views/MarketPlace/index.jsx
+++ src/Views/MarketPlace/index.jsx
@@ -4,8 +4,10 @@ import marketService from '../../Services/market.service';
 import Filters from './components/Filters';
 import Products from './components/Products';
 import SearchBar from './components/SearchBar';
+import {logOutAmplitude} from '../../Utils/amplitude';
 
 import styles from './styles.module.scss';
+import { useHistory } from 'react-router-dom';
 
 const MarketPlace = () => {
   const [filters, setFilters] = useState ({
@@ -18,23 +20,45 @@ const MarketPlace = () => {
   const [xPage, setxPage] = useState (25);
   const [input, setInput] = useState (1);
 
+  const history = useHistory()
+
   useEffect(()=> {
 
     ( async()=> {
-      const response = await marketService.getData();
-      let rarityItem = {}
-      response.rarityList.forEach(rarity => {
-        Object.defineProperty(rarityItem, rarity.name, {
-          configurable: true,
-          enumerable: true,
-          writable: true,
-          value: false,
-        })
-      });
-      setFilters(rarityItem)
+      try {
+        const response = await marketService.getData();
+        if (response.error.text !== "") {
+          if (response.error.text.includes("authorized")) {
+            alert("Session expired, please login again.");
+            localStorage.removeItem("userBP");
+            logOutAmplitude();
+            history.push("/");
+            window.location.reload();
+          } else {
+            alert(response.error.text);
+            return
+          }
+        }
+
+        let rarityItem = {}
+        response.rarityList.forEach(rarity => {
+          Object.defineProperty(rarityItem, rarity.name, {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: false,
+          })
+        });
+        setFilters(rarityItem)
+
+      } catch (error) {
+        alert(error.message)
+        return
+      }
+
     })()
     
-  }, [setFilters])
+  }, [setFilters, history])
 
   return (
     <Background>
