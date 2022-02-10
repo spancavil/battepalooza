95118d0b617e22945c131334f6087d3151062b86
spancavil <soancavil@gmail.com>
2022-02-09 23:21:40 -0300

    Filters applied successfully (BattleCount, rarity and Type)

---
+++ src/Assets/svg/FilterIcon.js
@@ -0,0 +1,42 @@
+import * as React from "react"
+
+const FilterIcon = (props) => (
+  <svg
+    width={28}
+    height={28}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M4.91 14.51V2.05M4.91 25.87v-7.106M23 16.341V2.05M23 25.87v-5.296"
+      stroke="#fff"
+      strokeWidth={2.8}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M13.955 4.431V2.05"
+      stroke="#fff"
+      strokeWidth={4}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M13.955 25.87V8.861"
+      stroke="#fff"
+      strokeWidth={2.8}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M2.447 14.51h4.574M20.713 16.355h4.573M11.668 8.276h4.573"
+      stroke="#fff"
+      strokeWidth={3.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default FilterIcon;
--- src/Views/MarketPlace/components/CollectionNfts/index.jsx
+++ src/Views/MarketPlace/components/CollectionNfts/index.jsx
@@ -18,13 +18,15 @@ const CollectionNfts = ({
   const [nftsFiltered, setNftFiltered] = useState (nfts);
 
   const breakpoint = useMediaQuery ('(max-width: 1200px)');
-
+  console.log(nftMarket);
   useEffect (
     () => {
       breakpoint ? setxPage (4) : setxPage (25);
     },
     [breakpoint, setxPage]
   );
+
+  //console.log(filters);
 
   useEffect (
     () => {
@@ -33,28 +35,59 @@ const CollectionNfts = ({
       let filtro2 = [];
       let filtro3 = [];
       let filtro4 = [];
+      let filtro5 = [];
+      let filtro6 = [];
+      let filtro7 = [];
+      let filtro8 = [];
+      let filtro9 = [];
+      let filtro10 = [];
 
       if (filters.COMMON)
         filtro1 = auxFilter.filter (nft => nft.rarity === 'COMMON');
-      if (filters.RARE) filtro2 = auxFilter.filter (nft => nft.rarity === 'RARE');
-      if (filters.EPIC) filtro3 = auxFilter.filter (nft => nft.rarity === 'EPIC');
-      if (filters.LEGENDARY)
+      if (filters.RARE) 
+        filtro2 = auxFilter.filter (nft => nft.rarity === 'RARE');
+      if (filters.EPIC) 
+        filtro3 = auxFilter.filter (nft => nft.rarity === 'EPIC');
+      if (filters.LEGENDARY) 
         filtro4 = auxFilter.filter (nft => nft.rarity === 'LEGENDARY');
-      if (
-        !filters.COMMON &&
-        !filters.RARE &&
-        !filters.EPIC &&
-        !filters.LEGENDARY
-      )
-        setNftFiltered (nftMarket);
-      else {
-        setNftFiltered ([...filtro1, ...filtro2, ...filtro3, ...filtro4]);
-      }
+      if (filters.Weapon)
+        filtro5 = auxFilter.filter (nft => nft.type === 2);
+      if (filters.Character)
+        filtro6 = auxFilter.filter (nft => nft.type === 1);
+      if (filters["0-100"])
+        filtro7 = auxFilter.filter (nft => nft.playCount <= 100)
+      if (filters["101-200"])
+        filtro8 = auxFilter.filter (nft => nft.playCount <= 200 && nft.playCount > 100)
+      if (filters["201-300"])
+        filtro9 = auxFilter.filter (nft => nft.playCount <= 300 && nft.playCount > 200)
+      if (filters["Over 300"])
+        filtro10 = auxFilter.filter (nft => nft.playCount > 300)
+
+      const filtroWeapon = !filters.Weapon && !filters.Character? 
+                                              [...nftMarket] 
+                                              :
+                                              [...filtro5, ...filtro6]
+    
+      const filtroRarity = !filters.COMMON && !filters.RARE && !filters.EPIC &&!filters.LEGENDARY ?
+                                              [...nftMarket]
+                                              :
+                                              [...filtro1, ...filtro2, ...filtro3, ...filtro4]
+
+      const filtroPlayCount = !filters["0-100"] && !filters["101-200"] && !filters["201-300"] && !filters["Over 300"] ?
+                                              [...nftMarket]
+                                              :
+                                              [...filtro7, ...filtro8, ...filtro9, ...filtro10]
+
+      //Colocamos los valores que coinciden en ambos filtros de búsqueda (es como un inner join)
+      const coincidencias = filtroWeapon.filter(value => filtroRarity.includes(value))
+                                        .filter(value => filtroPlayCount.includes(value))
+
+      setNftFiltered (coincidencias);
+      
     },
     [filters, nftMarket]
   );
 
-  console.log(nftMarket);
   const max = nftsFiltered.length / xPage;
 
   return (
