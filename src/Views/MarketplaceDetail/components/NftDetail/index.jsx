f8533fd8fd4a26f10ad6cb9c6c1a352094ad8693
ÂMauro069 <maurovera069@gmail.com>
2022-02-19 17:33:16 -0300

    Update market detail in desktop

--- src/Views/MarketplaceDetail/components/NftDetail/index.jsx
+++ src/Views/MarketplaceDetail/components/NftDetail/index.jsx
@@ -1,73 +1,65 @@
-import React, {useEffect, useState, useContext} from 'react';
-import styles from './styles.module.scss';
-import Button from '../../../../Global-Components/Button';
-import {useParams, useHistory} from 'react-router';
-import marketService from '../../../../Services/market.service';
-import Loader from '../../../../Global-Components/Loader';
-import { logOutAmplitude } from '../../../../Utils/amplitude';
-import { UserData } from '../../../../Context/UserProvider';
-import fireToast from '../../../../Utils/sweetAlert2';
+import React, { useEffect, useState, useContext } from "react";
+import styles from "./styles.module.scss";
+import Button from "../../../../Global-Components/Button";
+import { useParams, useHistory } from "react-router";
+import marketService from "../../../../Services/market.service";
+import Loader from "../../../../Global-Components/Loader";
+import { logOutAmplitude } from "../../../../Utils/amplitude";
+import { UserData } from "../../../../Context/UserProvider";
+import fireToast from "../../../../Utils/sweetAlert2";
 
-const NftDetail = ({nfts, setNft, setNftListing}) => {
+const NftDetail = ({ nfts, setNft, setNftListing }) => {
+  const history = useHistory();
 
-  const history = useHistory()
-
-  const {nftId} = useParams ();
+  const { nftId } = useParams();
   let nftSplitted = nftId.split("-");
   const uid = nftSplitted[0];
   const seller = nftSplitted[1];
 
-  const [chosenNft, setChosenNft] = useState ({});
+  const [chosenNft, setChosenNft] = useState({});
   const [loading, setLoading] = useState(false);
 
-  const {userData} = useContext(UserData);
+  const { userData } = useContext(UserData);
 
-  useEffect (
-    () => {
-
-      (async ()=> {
-        
-        const response = await marketService.getNftMarketplaceDetail(seller, uid);
-        if (response.error.text !== "") {
-          if (response.error.text.includes("authorized")) {
-            alert("Session expired, please login again.");
-            localStorage.removeItem("userBP");
-            logOutAmplitude();
-            history.push("/");
-            window.location.reload();
-          } else {
-            alert(response.error.text);
-            history.push("/");
-            window.location.reload();
-          }
+  useEffect(() => {
+    (async () => {
+      const response = await marketService.getNftMarketplaceDetail(seller, uid);
+      if (response.error.text !== "") {
+        if (response.error.text.includes("authorized")) {
+          alert("Session expired, please login again.");
+          localStorage.removeItem("userBP");
+          logOutAmplitude();
+          history.push("/");
+          window.location.reload();
+        } else {
+          alert(response.error.text);
+          history.push("/");
+          window.location.reload();
         }
+      }
 
-        setChosenNft (response.product);
-      })()
-
-    },
-    [uid, seller, history]
-  );
+      setChosenNft(response.product);
+    })();
+  }, [uid, seller, history]);
 
   useEffect(() => {
     setLoading(true);
   }, []);
 
   const handleBuy = () => {
-    if (Object.keys(userData).length !== 0)
-    setNft (chosenNft);
+    if (Object.keys(userData).length !== 0) setNft(chosenNft);
     else {
-      fireToast("Need login", 1200, '300px');
+      fireToast("Need login", 1200, "300px");
     }
   };
 
   const handleList = () => {
-     ('Click on list');
+    ("Click on list");
     setNftListing(chosenNft);
   };
 
   const goBack = () => {
-    history.goBack ();
+    history.goBack();
   };
 
   console.log(chosenNft);
@@ -77,62 +69,77 @@ const NftDetail = ({nfts, setNft, setNftListing}) => {
       <p className={styles.back} onClick={goBack}>
         &#60; Go back to Marketplace
       </p>
-      {chosenNft?.rarity !== undefined ? 
-      <div className={styles.detailContainer}>
-            <div className={styles.videoContainer}>
-            {chosenNft.movieUrl ?
-              <>
-              {loading && (
-                <div className={styles.loadMessageContainer}>
-                  <Loader/>
-                </div>
-              )}
-              <video  
-                onCanPlayThrough={() => setLoading(false)}
-                className={styles.pinVideo}
-                src={chosenNft.movieUrl}
-                muted
-                autoPlay
-                loop
-              />
-              </>
-              :
-              <div className={styles.loadMessageContainer}>
-                <h2 className={styles.loadMessage}>
-                  No video for this NFT
-                </h2>
-              </div>
-            }
-            </div>
 
-            <div className={styles.detailDescription}>
-              <h2 className={styles.title}>
-                {chosenNft.itemName} <br /> {chosenNft.repName}
-              </h2>
-              <h2 className={styles.seller}>
-                {chosenNft.rarity}
-              </h2>
-              <span className={styles.seller}>
-                Sold by <span className={styles.sellerYellow}>{chosenNft.sellerPid}</span>
-              </span>
-              <p className={styles.title}>
-                Price <span className={styles.price}>{chosenNft.price} NCoin </span>
-              </p>
-              <p className={styles.subTitle}>
-                Fee (5%) <span className={styles.price}>{chosenNft.fee} NCoin</span> 
-              </p>
-              {/* <span className={styles.seller}>
-                Lowest price {chosenNft.lowestPrice}
-                <br />
-                Highest price {chosenNft.highestPrice}
-              </span> */}
-              <div className={styles.buttonsContainer}>
-                <Button title="Buy" onClick={() => handleBuy ()} />
-                <Button title="Listing" onClick={() => handleList ()} />
+      <div className={styles.card}>
+        <div className={styles.text}>
+          <div className={styles.cardContainer}>
+            <div className={styles.topContainer}>
+              <div className={styles.videoContainer}>
+                {chosenNft?.rarity !== undefined && (
+                  <>
+                    {loading && (
+                      <div className={styles.loadMessageContainer}>
+                        <Loader />
+                      </div>
+                    )}
+                    <video
+                      onCanPlayThrough={() => setLoading(false)}
+                      className={styles.pinVideo}
+                      src={chosenNft.movieUrl}
+                      muted
+                      autoPlay
+                      loop
+                    />
+                  </>
+                )}
+              </div>
+              <div className={styles.topRightContainer}>
+                <div className={styles.cont1}>
+                  <div className={styles.cont2}>
+                    <div className={styles.cont2a}>
+                      <p className={styles.priceTitle}>Price:</p>
+                      <p className={styles.price}>{chosenNft.price} NCoin</p>
+                      <div className={styles.feeCont}>
+                        <p className={styles.feeTitle}>Fee:</p>
+                        <p className={styles.fee}>{chosenNft.fee}</p>
+                        <p>NCoin</p>
+                      </div>
+                    </div>
+                    <div className={styles.cont2b}>
+                      <p className={styles.soldbyTitle}>Sold by:</p>
+                      <div className={styles.soldbyCont}>
+                        <p className={styles.soldby}>{chosenNft.sellerName}</p>
+                        <p className={styles.soldbyPID}>
+                          #{chosenNft.sellerPid}
+                        </p>
+                      </div>
+                    </div>
+                    <div className={styles.cont2c}>
+                      <Button onClick={handleBuy} width={'200px'} title={'BUY'} />
+                      <Button onClick={handleList} width={'200px'} title={'LISTING'} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.bottomContainer}>
+              <div className={styles.bottomLeftContainer}>
+                <div className={styles.item}>
+                  <p className={styles.itemName}>{chosenNft.itemName}</p>
+                  <p className={styles.repName}>[ {chosenNft.repName} ]</p>
+                </div>
+
+                <div>
+                  <p className={styles.rarity}>Rarity: {chosenNft.rarity}</p>
+                </div>
+              </div>
+              <div className={styles.button}>
+                <div> {/* {chosenNft.price} */}</div>
               </div>
             </div>
           </div>
-        : null}
+        </div>
+      </div>
     </>
   );
 };
