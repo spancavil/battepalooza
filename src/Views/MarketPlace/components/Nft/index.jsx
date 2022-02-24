d19c27adb9f33734227c79afa94cc863519e2af0
ÂMauro069 <maurovera069@gmail.com>
2022-02-24 11:58:39 -0300

    Fix warnings

--- src/Views/MarketPlace/components/Nft/index.jsx
+++ src/Views/MarketPlace/components/Nft/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Loader from "../../../../Global-Components/Loader";
 import styles from "./styles.module.scss";
 
 const Nft = ({ nft, tilts, index }) => {
@@ -26,54 +25,3 @@ const Nft = ({ nft, tilts, index }) => {
 
 export default Nft;
 
-{
-  /* <Link
-style={{textDecoration: 'none', overflow: 'visible'}}
-to={`/marketplace/${nft.uniqueId}-${nft.sellerPid}`}
-key={nft.uniqueId}
->
-    <div className={styles.cardNft} key={nft.uniqueId} 
-    onMouseEnter={()=>setFlagVideo(true)} 
-    onMouseLeave ={()=> setFlagVideo(false)}>
-        <div className={styles.videoContainer}>
-            {flagVideo ? 
-                nft.movieUrl?
-                <>
-                    {loading && (
-                    <div className={styles.loadMessageContainer}>
-                        <Loader />
-                    </div>
-                    )} 
-                    <video
-                    onCanPlayThrough={()=>setLoading(false)}
-                    className={styles.pinVideo}
-                    src={nft.movieUrl}
-                    muted
-                    autoPlay
-                    loop
-                    />
-                </>
-                :
-                <div className={styles.loadMessageContainer}>
-                    <h2 className={styles.loadMessage}>
-                        No video for this NFT
-                    </h2>
-                </div>
-            :
-            <img
-            className={styles.imgNft}
-            src={nft.thumbnailUrl}
-            alt="nft"
-            />
-            }
-        </div>
-        
-        <div className={styles.texts}>
-            <p>{nft.itemName}</p>
-            <p className={styles.text2}>{nft.rarity}</p>
-            <p>#{nft.serial}</p>
-            <p className={styles.price}>{nft.price} NCoin</p>
-        </div>
-    </div>
-</Link> */
-}
