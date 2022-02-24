c7a6de0fde407a2f09bb5408c214e4e3a3f7797f
ÂMauro069 <maurovera069@gmail.com>
2022-02-24 11:12:13 -0300

    Delete paths that were not used

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import HomeContainer from './Views/Home';
-import NavBar from './Views/Navbar';
-import Error404 from './Views/Error404';
-import Account from './Views/Account';
-import SignUp from './Views/SignUp';
-import Login from './Views/Login';
-import Verification from './Views/Verification';
-import NeedLogin from './Views/NeedLogin';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomeContainer from "./Views/Home";
+import NavBar from "./Views/Navbar";
+import Error404 from "./Views/Error404";
+import Account from "./Views/Account";
+import SignUp from "./Views/SignUp";
+import Login from "./Views/Login";
+import Verification from "./Views/Verification";
+import NeedLogin from "./Views/NeedLogin";
 import Collection from "./Views/Collection";
-import CollectionDetail from './Views/CollectionDetail';
-import Privacy from './Views/PrivacyAndTerms/Privacy';
-import TermsOfUse from './Views/PrivacyAndTerms/TermsOfUse';
-import MarketPlace from './Views/MarketPlace';
-import MarketplaceDetail from './Views/MarketplaceDetail';
-import {useContext} from 'react';
-import {UserData} from './Context/UserProvider';
-import Drop from './Views/Drop';
-import DropDetail from './Views/DropDetail';
+import CollectionDetail from "./Views/CollectionDetail";
+import Privacy from "./Views/PrivacyAndTerms/Privacy";
+import TermsOfUse from "./Views/PrivacyAndTerms/TermsOfUse";
+import MarketPlace from "./Views/MarketPlace";
+import MarketplaceDetail from "./Views/MarketplaceDetail";
+import { useContext } from "react";
+import { UserData } from "./Context/UserProvider";
+import Drop from "./Views/Drop";
+import DropDetail from "./Views/DropDetail";
 
-import './Styles/Global.scss';
+import "./Styles/Global.scss";
 
-function App () {
-  const {userData, error404} = useContext (UserData);
+function App() {
+  const { userData, error404 } = useContext(UserData);
 
   return (
     <BrowserRouter>
@@ -33,15 +33,8 @@ function App () {
         <Route exact path="/verification" component={Verification} />
         <Route exact path="/needlogin" component={NeedLogin} />
         <Route exact path="/account">
-          {Object.keys (userData).length === 0 ? <NeedLogin /> : <Account />}
+          {Object.keys(userData).length === 0 ? <NeedLogin /> : <Account />}
         </Route>
-        {/* <Route exact path="/packs" component={Packs} />
-        <Route exact path="/packs/:id" component={PackDetail} />
-        <Route exact path="/join-drop" component={JoinDrop} />
-        <Route exact path="/open-pack" component={OpenPack} /> */}
-        {/* <Route exact path="/my-packs" component={MyPacks} />
-        <Route exact path="/my-skins" component={MyNfts} /> */}
-        {/* <Route exact path="/card-animation" component={CardAnimation} /> */}
         <Route exact path="/drop" component={Drop} />
         <Route exact path="/drop/:id" component={DropDetail} />
         <Route exact path="/collection" component={Collection} />
