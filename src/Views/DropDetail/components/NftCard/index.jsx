0ee89ad1ee1f4aea288b7bd352fbc54888e393a0
ÂMauro069 <maurovera069@gmail.com>
2022-03-09 17:16:16 -0300

    Drop detail in progress

--- src/Views/DropDetail/components/NftCard/index.jsx
+++ src/Views/DropDetail/components/NftCard/index.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
-import styles from './styles.module.scss';
+import Button from "../../../../Global-Components/Button";
+import styles from "./styles.module.scss";
+import COMMON from "../../Assets/Tiericon_common.png";
+import EPIC from "../../Assets/Tiericon_Epic.png";
+import LEGENDARY from "../../Assets/Tiericon_Legendary.png";
 
 const NftCard = ({ nft }) => {
+  console.log(nft);
 
-    console.log(nft);
+  const rarityImg = () => {};
 
-    return (
-        <div className={styles.cardDropDetail}>
-            <div></div>
+  return (
+    <div className={styles.cardDropDetail}>
+      <div className={styles.cardTop}>
+        <div className={styles.rarityContainer}>
+          <img
+            src={
+              nft.rarity === "COMMON"
+                ? COMMON
+                : nft.rarity === "EPIC"
+                ? EPIC
+                : LEGENDARY
+            }
+            alt={nft.rarity}
+          />
+          <p>{nft.rarity}</p>
         </div>
-    )
-}
+        <p className={styles.amount}>
+          {nft.leftAmount} / {nft.limitAmount}
+        </p>
+      </div>
+      <div className={styles.imgContainer}>
+        <img src={nft.thumbnailUrl} alt={nft.itemName} />
+      </div>
+      <h3>{nft.itemName}</h3>
+      <Button title={`Buy ${nft.price} NCoin`} />
+    </div>
+  );
+};
 
-export default NftCard
+export default NftCard;
--- src/Views/DropDetail/index.jsx
+++ src/Views/DropDetail/index.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState, useRef, useContext } from 'react';
-import { useParams } from 'react-router-dom';
-import Background from '../../Global-Components/Background';
-import Button from '../../Global-Components/Button';
-import styles from './styles.module.scss';
+import React, { useEffect, useState, useRef, useContext } from "react";
+import { useParams } from "react-router-dom";
+import Background from "../../Global-Components/Background";
+import Button from "../../Global-Components/Button";
+import styles from "./styles.module.scss";
 // import { dropsList } from '../../Services/dropList';
-import { useHistory } from 'react-router-dom';
-import Modal from '../../Global-Components/Modal';
-// import NftCard from './components/NftCard';
-import { fireAlertAsync } from '../../Utils/sweetAlert2';
-import { logOutAmplitude } from '../../Utils/amplitude';
-import dropService from '../../Services/drop.service';
+import { useHistory } from "react-router-dom";
+import Modal from "../../Global-Components/Modal";
+import NftCard from "./components/NftCard";
+import { fireAlertAsync } from "../../Utils/sweetAlert2";
+import { logOutAmplitude } from "../../Utils/amplitude";
+import dropService from "../../Services/drop.service";
 
-import heroBannerImage from '../../Assets/img/dropHeroBg.png';
-import { UserData } from '../../Context/UserProvider';
+import heroBannerImage from "../../Assets/img/dropHeroBg.png";
+import { UserData } from "../../Context/UserProvider";
 
 const DropDetail = () => {
   const [dropSelected, setDropSelected] = useState();
@@ -22,34 +22,32 @@ const DropDetail = () => {
 
   const { userData } = useContext(UserData);
 
-  const divHero = useRef()
+  const divHero = useRef();
 
   //Set background with useRef
   useEffect(() => {
     if (dropSelected) {
       divHero.current.style.background = `url(${heroBannerImage})`;
-      divHero.current.style.backgroundSize = 'cover';
+      divHero.current.style.backgroundSize = "cover";
     }
-  }, [divHero, dropSelected])
+  }, [divHero, dropSelected]);
 
   useEffect(() => {
-
     const fetchData = async () => {
       try {
         const dropDetail = await dropService.getDropDetail(
           userData?.pid ? userData.pid : "",
-          parseInt(id),
-        )
+          parseInt(id)
+        );
 
         if (dropDetail.error.text !== "") {
           if (dropDetail.error.text.includes("authorized")) {
-            fireAlertAsync("Session expired, please login again.")
-              .then(() => {
-                localStorage.removeItem("userBP");
-                logOutAmplitude();
-                history.push("/");
-                window.location.reload();
-              })
+            fireAlertAsync("Session expired, please login again.").then(() => {
+              localStorage.removeItem("userBP");
+              logOutAmplitude();
+              history.push("/");
+              window.location.reload();
+            });
           } else {
             fireAlertAsync("Oops an error ocurred", dropDetail.error.text);
           }
@@ -59,47 +57,54 @@ const DropDetail = () => {
       } catch (error) {
         fireAlertAsync(error.message);
       }
-    }
+    };
 
     fetchData();
-
   }, [setDropSelected, userData, history, id]);
 
-  console.log(id);
   console.log(dropSelected);
 
   return (
     <Background>
-      {dropSelected
-        ? <>
+      {dropSelected ? (
+        <>
           <p className={styles.back} onClick={() => history.goBack()}>
             &#60; Go back
           </p>
           <div className={styles.container}>
             <div className={styles.rectangle}>
-            
-            <div className={styles.hero} ref={divHero}>
-            </div>
+              <div className={styles.hero} ref={divHero}>
+                <div className={styles.information}>
+                  <h2 className={styles.release}>Release Date</h2>
+                  <h2 className={styles.name}>Battlepalooza Series 1 Drop 1</h2>
+                </div>
+              </div>
+              <div className={styles.olderTitle}>
+                <h3 className={styles.older}>What is in this drop?</h3>
+              </div>
 
-            {/* <div className={styles.nftsContainer}>
-              {dropSelected?.content?.map(nft => {
-                return <NftCard nft={nft} key={nft.uniqueId} />
-              })}
-            </div> */}
-
+              <div className={styles.nftsContainer}>
+                {dropSelected?.nftProducts?.map((nft) => {
+                  return <NftCard nft={nft} key={nft.id} />;
+                })}
+              </div>
             </div>
           </div>
         </>
-        : <p>Loading</p>}
-      {open
-        ? <div className={styles.modalContainer}>
-          <Modal handleClose={() => setOpen(!open)} title={'Checkout'}>
-            <p className={styles.modalText}>Will you use 20.000 Ncoin to buy Rare Pack?</p>
+      ) : (
+        <p>Loading</p>
+      )}
+      {open ? (
+        <div className={styles.modalContainer}>
+          <Modal handleClose={() => setOpen(!open)} title={"Checkout"}>
+            <p className={styles.modalText}>
+              Will you use 20.000 Ncoin to buy Rare Pack?
+            </p>
             <Button title="BUY" />
             <p className={styles.chargeNow}>Not enough Ncoin? Charge now</p>
           </Modal>
         </div>
-        : null}
+      ) : null}
     </Background>
   );
 };
