88adadb68bec70b214c58b7125155574df490dc9
ÂMauro069 <maurovera069@gmail.com>
2022-03-14 17:43:18 -0300

    Add borders

--- src/Views/MarketPlace/components/Nft/index.jsx
+++ src/Views/MarketPlace/components/Nft/index.jsx
@@ -2,21 +2,33 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 const Nft = ({ nft, tilt, onClick }) => {
-
+  console.log(nft.rarity);
   return (
     <div
       ref={tilt}
-      className={styles.cardNft}
-      key={nft.itemId}
-      onClick={() => onClick(nft.uniqueId, nft.sellerPid)}
+      className={
+        nft.rarity === "COMMON"
+          ? styles.borderCommon
+          : nft.rarity === "RARE"
+          ? styles.borderRare
+          : nft.rarity === "EPIC"
+          ? styles.borderEpic
+          : styles.borderLegendary
+      }
     >
-      {/* El source luego cambiara en base al asset */}
-      <img className={styles.imgNft} src={nft.thumbnailUrl} alt="nft-thumb" />
-      <div className={styles.texts}>
-        <p>{nft.repName}</p>
-        <p>{nft.itemName}</p>
-        <p className={styles.text2}>{nft.rarity}</p>
-        <p className={styles.price}>{nft.price} NCoin</p>
+      <div
+        className={styles.cardNft}
+        key={nft.itemId}
+        onClick={() => onClick(nft.uniqueId, nft.sellerPid)}
+      >
+        {/* El source luego cambiara en base al asset */}
+        <img className={styles.imgNft} src={nft.thumbnailUrl} alt="nft-thumb" />
+        <div className={styles.texts}>
+          <p>{nft.repName}</p>
+          <p>{nft.itemName}</p>
+          <p className={styles.text2}>{nft.rarity}</p>
+          <p className={styles.price}>{nft.price} NCoin</p>
+        </div>
       </div>
     </div>
   );
