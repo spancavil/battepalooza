b22493fed261b5aa558b82c20139977792063092
spancavil <soancavil@gmail.com>
2022-03-18 12:07:45 -0300

    Added drop timer in HD

--- src/Views/Drop/index.jsx
+++ src/Views/Drop/index.jsx
@@ -7,28 +7,46 @@ import Button from '../../Global-Components/Button';
 import { NftData } from '../../Context/NftProvider';
 import heroBannerImage from '../../Assets/img/dropHeroBg.png';
 import heroBannerSecondary from '../../Assets/img/bg-secondary-drop.png'
+import { getDaysMinutesSeconds } from '../../Utils/createDate';
 
 const Drop = () => {
   const charEsc = '<';
 
-  const {drops} = useContext(NftData);
+  const { drops } = useContext(NftData);
 
   const history = useHistory();
   const [olderDrops, setOlderDrops] = useState([]);
   const [hero, setHero] = useState({});
+  const [timerRelease, setTimerRelease] = useState({ message: "", state: "" })
 
   const divHero = useRef()
   const divsOlder = useMemo(() => (olderDrops.map(() => createRef())), [olderDrops])
 
   //set data for drops
   useEffect(() => {
-    if (drops.length !== 0){
+    let intervalTimer;
+    if (drops.length !== 0) {
       const hero = drops.mainDrop;
       const olderDrops = drops.prevDropList;
       setHero(hero)
       setOlderDrops(olderDrops)
+
+      //Sandbox
+      // const {message, state} = getDaysMinutesSeconds(Date.now() + 2016000010, Date.now() + 172800000);
+      intervalTimer = setInterval(() => {
+        const date = getDaysMinutesSeconds(drops.mainDrop.startTime, drops.mainDrop.endTime)
+        const { message, state } = date;
+        console.log(state);
+        setTimerRelease({ message, state });
+        if (state !== ("active" || "willBeActive")) clearInterval(intervalTimer);
+      }, 1000
+      )
     }
-  }, [setHero, setOlderDrops, drops])
+    return () => {
+      console.log("Unmounted Drop");
+      clearInterval(intervalTimer)
+    }
+  }, [setHero, setOlderDrops, drops, setTimerRelease])
 
   //Set backgrounds with useRef
   useEffect(() => {
@@ -50,8 +68,6 @@ const Drop = () => {
     history.push(`/drop/1`)
   }
 
-  console.log(drops);
-
   return (
     <Background>
       <div className={styles.content2}>
@@ -64,14 +80,19 @@ const Drop = () => {
               <h2 className={styles.release} style={{ padding: '55px 0px 8px 0px' }}>Release date {new Date(hero?.startTime).toLocaleDateString()}</h2>
               <h2 className={styles.name} style={{ padding: '0px 0px 12px 0px' }}>{hero?.name}</h2>
               <h2 className={styles.roboto} style={{ padding: '0px 0px 20px 0px' }}>{hero?.desc} </h2>
-              <Button
-                title="Find out more"
-                width={'227px'}
-                onClick={handleFindOut}
-                style={{
-                  fontSize: '24px'
-                }}
-              />
+              <div className={styles.findOut}>
+                <Button
+                  title="Find out more"
+                  width={'227px'}
+                  onClick={handleFindOut}
+                  style={{
+                    fontSize: '24px'
+                  }}
+
+                />
+                <h5 className={styles.available}>{timerRelease.message}</h5>
+
+              </div>
             </div>
             <div className={styles.mainImages}>
               {/* {hero?.content?.map(nft => {
