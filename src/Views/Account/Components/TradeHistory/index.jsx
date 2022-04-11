89e330d1a2ce35aa12bbda78666999cf6866a3af
Mauro Vera <Maurovera069@gmail.com>
2022-04-11 15:21:14 -0300

    Connect Api

--- src/Views/Account/Components/TradeHistory/index.jsx
+++ src/Views/Account/Components/TradeHistory/index.jsx
@@ -1,9 +1,7 @@
-import { useState, useEffect, useContext} from "react";
+import { useState, useEffect, useContext, useCallback } from "react";
 import Pagination from "../../../../Global-Components/Pagination";
 import { useMediaQuery } from "../../../../Hooks/useMediaQuery";
 import NftDetail from "../NftDetail";
-
-import { TradeDataDummy } from "../../Data";
 
 import styles from "./styles.module.scss";
 import authService from "../../../../Services/auth.service";
@@ -12,8 +10,8 @@ import { UserData } from "../../../../Context/UserProvider";
 const TradeHistory = ({ page, setPage, setxPage, xPage, input, setInput }) => {
   const [tradeDetail, setTradeDetail] = useState(false);
   const [category, setCategory] = useState("purchase");
-  // const [tradeData, setTradeData] = useState(null);
-  const {userData} = useContext(UserData);
+  const [tradeData, setTradeData] = useState(null);
+  const { userData } = useContext(UserData);
 
   const breakpoint = useMediaQuery("(max-width: 576px)");
 
@@ -22,16 +20,32 @@ const TradeHistory = ({ page, setPage, setxPage, xPage, input, setInput }) => {
     setPage(1);
   }, [breakpoint, setxPage, setInput, setPage]);
 
-  const max = TradeDataDummy.length / xPage;
+  const max = tradeData?.length / xPage;
 
-  useEffect(()=> {
-    (async ()=> {
-      const response = await authService.tradeHistoryList(userData.bpToken, userData.pid);
-      console.log(response);
-      const response2 = await authService.tradeHistoryDetail(userData.bpToken, userData.pid, "73e68ad6-bd8c-4a04-8f83-86c30da4d314");
-      console.log(response2);
-    })()
-  }, [userData])
+  const getTradeHistory = useCallback(async () => {
+    const response = await authService.tradeHistoryList(
+      userData.bpToken,
+      userData.pid
+    );
+
+    setTradeData(response.asBuyer.historyList);
+
+    if (category === "sold") {
+      setTradeData(response.asSeller.historyList);
+    }
+
+    const response2 = await authService.tradeHistoryDetail(
+      userData.bpToken,
+      userData.pid,
+      "73e68ad6-bd8c-4a04-8f83-86c30da4d314"
+    );
+
+    console.log({ response, response2, tradeData });
+  }, [category, tradeData, userData.bpToken, userData.pid]);
+
+  useEffect(() => {
+    getTradeHistory();
+  }, [userData, category]);
 
   return (
     <div className={styles.container}>
@@ -45,7 +59,9 @@ const TradeHistory = ({ page, setPage, setxPage, xPage, input, setInput }) => {
             Purchase
           </span>
           <span
-            onClick={() => setCategory("sold")}
+            onClick={() => {
+              setCategory("sold");
+            }}
             className={category === "sold" ? styles.active : ""}
           >
             Sold
@@ -54,46 +70,46 @@ const TradeHistory = ({ page, setPage, setxPage, xPage, input, setInput }) => {
       </div>
 
       <div className={styles.trades}>
-        {TradeDataDummy.slice(
-          (page - 1) * xPage,
-          (page - 1) * xPage + xPage
-        ).map((trade, i) => (
-          <>
-            <div key={i} className={styles.tradeContainer}>
-              <div className={styles.trade}>
-                <div className={styles.item}>
-                  <p>Date</p>
-                  <span>{trade.date}</span>
-                </div>
+        {tradeData &&
+          tradeData
+            .slice((page - 1) * xPage, (page - 1) * xPage + xPage)
+            .map((trade, i) => (
+              <div key={i}>
+                <div className={styles.tradeContainer}>
+                  <div className={styles.trade}>
+                    <div className={styles.item}>
+                      <p>Date</p>
+                      <span>{trade.soldAt}</span>
+                    </div>
 
-                <div className={styles.item}>
-                  <p>Name</p>
-                  <span>{trade.name}</span>
-                </div>
+                    <div className={styles.item}>
+                      <p>Name</p>
+                      <span>{trade.itemId}</span>
+                    </div>
 
-                <div className={styles.item}>
-                  <p>{category === "purchase" ? "Seller" : "Buyer"} </p>
-                  <span>{trade.buyer}</span>
-                </div>
+                    <div className={styles.item}>
+                      <p>{category === "purchase" ? "Seller" : "Buyer"} </p>
+                      <span>{trade.buyerName}</span>
+                    </div>
 
-                <div className={styles.item}>
-                  <p>Price</p>
-                  <span>{trade.price} NCoin</span>
-                </div>
+                    <div className={styles.item}>
+                      <p>Price</p>
+                      <span>{trade.price} NCoin</span>
+                    </div>
 
-                <button onClick={() => setTradeDetail(true)}>VIEW</button>
+                    <button onClick={() => setTradeDetail(true)}>VIEW</button>
+                  </div>
+
+                  <div className={styles.line} />
+                </div>
+                {tradeDetail && (
+                  <NftDetail
+                    chosenNft={trade}
+                    handleClose={() => setTradeDetail(false)}
+                  />
+                )}
               </div>
-
-              <div className={styles.line} />
-            </div>
-            {tradeDetail && (
-              <NftDetail
-                chosenNft={trade}
-                handleClose={() => setTradeDetail(false)}
-              />
-            )}
-          </>
-        ))}
+            ))}
       </div>
 
       <div className={styles.pagination}>
