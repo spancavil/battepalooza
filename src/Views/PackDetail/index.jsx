bc657f950460f27e30281bf5765ee1b266530655
Mauro Vera <Maurovera069@gmail.com>
2022-07-19 12:53:55 -0300

    Remove the security of packs, so you can enter without being logged in

--- src/Views/PackDetail/index.jsx
+++ src/Views/PackDetail/index.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import {Redirect} from 'react-router';
-import Background from '../../Global-Components/Background';
-import SocialMedia from '../Home/Components/SocialMedia';
-import CardDetail from './Components/CardDetail';
-import styles from './styles.module.scss';
+import React from "react";
+import { Redirect } from "react-router";
+import Background from "../../Global-Components/Background";
+import SocialMedia from "../Home/Components/SocialMedia";
+import CardDetail from "./Components/CardDetail";
+import styles from "./styles.module.scss";
 
 /* NO SE ESTA USANDO ESTE COMPONENTE, E IBA EN LA RUTA /packs/:id */
 const PackDetail = () => {
-  const userStorage = JSON.parse (localStorage.getItem ('userBP'));
+  const userStorage = JSON.parse(localStorage.getItem("userBP"));
 
-  return !userStorage
-    ? <Redirect to="/needlogin" />
-    : <Background>
-        <div className={styles.container}>
-          <CardDetail />
-        </div>
-        <SocialMedia />
-      </Background>;
+  return (
+    <Background>
+      <div className={styles.container}>
+        <CardDetail />
+      </div>
+      <SocialMedia />
+    </Background>
+  );
 };
 
 export default PackDetail;
--- src/Views/Packs/index.jsx
+++ src/Views/Packs/index.jsx
@@ -1,70 +1,67 @@
-import React, {useContext, useState} from 'react';
-import Background from '../../Global-Components/Background';
-import SocialMedia from '../Home/Components/SocialMedia';
-import styles from './styles.module.scss';
-import Card from '../../Global-Components/Card';
-import {CardData} from '../../Context/CardDataProvider';
-import {useMediaQuery} from '../../Hooks/useMediaQuery';
-import {useHistory} from 'react-router-dom';
-import {Redirect} from 'react-router';
-import ScrollBar from '../../Global-Components/ScrollBar';
+import React, { useContext, useState } from "react";
+import Background from "../../Global-Components/Background";
+import SocialMedia from "../Home/Components/SocialMedia";
+import styles from "./styles.module.scss";
+import Card from "../../Global-Components/Card";
+import { CardData } from "../../Context/CardDataProvider";
+import { useMediaQuery } from "../../Hooks/useMediaQuery";
+import { useHistory } from "react-router-dom";
+import { Redirect } from "react-router";
+import ScrollBar from "../../Global-Components/ScrollBar";
 
 /* NO SE ESTA USANDO ESTE COMPONENTE, E IBA EN LA RUTA /packs */
 const Packs = () => {
-  const {packs, setPack} = useContext (CardData);
-  const [scroll, setScroll] = useState ({scrollLeft: '', scrollWidth: ''});
-  const queryTablet = useMediaQuery ('(max-width: 766px)');
-  const userStorage = JSON.parse (localStorage.getItem ('userBP'));
+  const { packs, setPack } = useContext(CardData);
+  const [scroll, setScroll] = useState({ scrollLeft: "", scrollWidth: "" });
+  const queryTablet = useMediaQuery("(max-width: 766px)");
+  const userStorage = JSON.parse(localStorage.getItem("userBP"));
 
-  const history = useHistory ();
+  const history = useHistory();
 
-  const setSelectedCard = packId => {
-    setPack (packId);
-    history.push (`/packs/${packId}`);
+  const setSelectedCard = (packId) => {
+    setPack(packId);
+    history.push(`/packs/${packId}`);
   };
 
-  const handleScroll = e => {
-    setScroll ({
+  const handleScroll = (e) => {
+    setScroll({
       scrollLeft: e.target.scrollLeft,
       scrollWidth: e.target.scrollWidth - e.target.offsetWidth,
     });
   };
 
-  return !userStorage
-    ? <Redirect to="/needlogin" />
-    : <Background>
-        <div className={styles.packContainer}>
-          <div className={styles.banner}>
-            BANNER IMG
-          </div>
-          <h4>
-            SKINS PACKS
-          </h4>
-          <div className={styles.cardContainer} onScroll={handleScroll}>
-            {packs.map (pack => {
-              return (
-                <Card
-                  key={pack.id}
-                  imgSrc={pack.imgSrc}
-                  text1={pack.description.text1}
-                  text2={pack.description.text2}
-                  text3={pack.description.text3}
-                  soldOut={pack.soldOut}
-                  sale={pack.sale}
-                  handleClick={() => setSelectedCard (pack.id)}
-                />
-              );
-            })}
-          </div>
-          {queryTablet &&
-            <ScrollBar
-              width={scroll.scrollWidth}
-              position={scroll.scrollLeft}
-              elements={packs.length}
-            />}
-          <SocialMedia />
+  return (
+    <Background>
+      <div className={styles.packContainer}>
+        <div className={styles.banner}>BANNER IMG</div>
+        <h4>SKINS PACKS</h4>
+        <div className={styles.cardContainer} onScroll={handleScroll}>
+          {packs.map((pack) => {
+            return (
+              <Card
+                key={pack.id}
+                imgSrc={pack.imgSrc}
+                text1={pack.description.text1}
+                text2={pack.description.text2}
+                text3={pack.description.text3}
+                soldOut={pack.soldOut}
+                sale={pack.sale}
+                handleClick={() => setSelectedCard(pack.id)}
+              />
+            );
+          })}
         </div>
-      </Background>;
+        {queryTablet && (
+          <ScrollBar
+            width={scroll.scrollWidth}
+            position={scroll.scrollLeft}
+            elements={packs.length}
+          />
+        )}
+        <SocialMedia />
+      </div>
+    </Background>
+  );
 };
 
 export default Packs;
