7d55aa4fcbc051a978ce8bc10511fc9b0cf2177f
Mauro Vera <Maurovera069@gmail.com>
2022-07-28 11:16:35 -0300

    Add button in pack detail

--- src/Views/PackDetail/Components/CardDetail/index.jsx
+++ src/Views/PackDetail/Components/CardDetail/index.jsx
@@ -2,17 +2,31 @@ import React, { useContext, useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import { useParams } from "react-router";
 import { CardData } from "../../../../Context/CardDataProvider";
+import Button from "../../../../Global-Components/Button";
+import fireToast from "../../../../Utils/sweetAlert2";
+import { UserData } from "../../../../Context/UserProvider";
 
 const CardDetail = () => {
   const { packs, setPack } = useContext(CardData);
   const { id } = useParams();
   const [pack, setSelectedPack] = useState();
+  const { userData } = useContext(UserData);
 
   useEffect(() => {
     const selectedPack = packs.find((pack) => pack.id === Number(id));
     setSelectedPack(selectedPack);
     setPack(selectedPack);
   }, [id, packs, setPack]);
+
+  const handleBuy = () => {
+    if (Object.keys(userData).length !== 0) {
+      console.log('Podes comprar')
+      /* setNft(chosenNft); */
+      /* sendAmplitudeData("Buy request Packs"); */
+    } else {
+      fireToast("Need login", 1200, "300px");
+    }
+  };
 
   return (
     <>
@@ -32,6 +46,7 @@ const CardDetail = () => {
               <li>{pack.content[1]}</li>
             </ul>
             <h3 className={styles.price}>Price {pack.price} NCoin</h3>
+            <Button onClick={handleBuy} title={"BUY"} />
           </div>
         </div>
       )}
