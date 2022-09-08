e022fc9a3fea9d5b236f3702f19983af81469b95
Mauro Vera <Maurovera069@gmail.com>
2022-09-08 11:53:53 -0300

    Connect by api

--- src/Views/PackDetailV2/components/PackInfo/index.jsx
+++ src/Views/PackDetailV2/components/PackInfo/index.jsx
@@ -1,8 +1,21 @@
+import { UserData } from "../../../../Context/UserProvider";
+import { useContext } from "react";
+
+import checkErrorMiddleware from "../../../../Utils/checkErrorMiddleware";
+import walletService from "../../../../Services/wallet.service";
 import NftCard from "../../../../Global-Components/NftCard";
 
 import styles from "./styles.module.scss";
+import fireToast, {
+  fireAlert,
+  fireAlertAsync,
+} from "../../../../Utils/sweetAlert2";
+import { useHistory } from "react-router-dom";
 
-export const PackInfo = ({ pack }) => {
+export const PackInfo = ({ pack, setCheckoutNCoin }) => {
+  const { userData } = useContext(UserData);
+  const history = useHistory();
+
   const nftsTest = [
     {
       uniqueId: "983235971287250685025551072688264024413005647533",
@@ -82,6 +95,77 @@ export const PackInfo = ({ pack }) => {
     },
   ];
 
+  const handleBuyNCoins = () => {
+    setCheckoutNCoin(true);
+  };
+
+  const getTransactions = async () => {
+    const response = await walletService.getWalletCryptoTransactions(
+      userData.bpToken,
+      userData.pid
+    );
+    //Obtenemos todas las transacciones
+    const { transactions } = response;
+
+    //En caso que hayan transacciones
+    if (transactions?.length) {
+      const sortedTx = response?.transactions.sort((a, b) => {
+        if (a.created > b.created) return 1;
+        if (a.created < b.created) return -1;
+        return 0;
+      });
+      console.log(sortedTx);
+      //Obtenemos la última transacción (que es la que debemos ir haciendo poll para consultar el estado)
+      const lastTx = sortedTx.pop();
+      await fireAlertAsync(
+        "Last transaction",
+        `Your last transaction id is: ${lastTx?.orderId}`,
+        "500px"
+      );
+      history.push("/open-pack");
+
+      //Si no hay tx devolvemos a Home
+    } else {
+      await fireAlertAsync("No transactions registered", "", "500px");
+      history.push("/");
+    }
+  };
+
+  const handleBuy = async () => {
+    if (Object.keys(userData).length !== 0) {
+      try {
+        const response = await walletService.getWalletToken(
+          userData.bpToken,
+          userData.email,
+          userData.pid,
+          pack?.id
+        );
+        const canContinue = checkErrorMiddleware(response, history);
+        if (canContinue) {
+          const { token } = response;
+          const popup = window.open(
+            `${process.env.REACT_APP_NWAY_URL}token=${token}&${process.env.REACT_APP_NWAY_URL_TITLE}`,
+            "Wallet payment",
+            "top=50,left= 200,width=800,height=620"
+          );
+
+          //Intervalo para detectar el cierre del popup
+          const popupTick = setInterval(() => {
+            if (popup.closed) {
+              console.log("se cerro la window");
+              clearInterval(popupTick);
+              getTransactions();
+            }
+          }, 500);
+        }
+      } catch (error) {
+        fireAlert("Oops, an error ocurred", error.message, "500px");
+      }
+    } else {
+      fireToast("Need login", 1200, "300px");
+    }
+  };
+
   return (
     <div className={styles.packInfo}>
       <div className={styles.left}>
@@ -107,8 +191,12 @@ export const PackInfo = ({ pack }) => {
           Welcome to Battlepalooza! Get a [{pack?.packName}] and start
           collecting the NFTs!
         </span>
-        <button className={styles.buy}>Buy</button>
-        <button className={styles.buyNcoin}>Buy with ncoins</button>
+        <button onClick={handleBuy} className={styles.buy}>
+          Buy
+        </button>
+        <button onClick={handleBuyNCoins} className={styles.buyNcoin}>
+          Buy with ncoins
+        </button>
       </div>
     </div>
   );
--- src/Views/PackDetailV2/index.jsx
+++ src/Views/PackDetailV2/index.jsx
@@ -1,19 +1,28 @@
 import { useContext, useEffect, useState } from "react";
 import { PackData } from "../../Context/PackProvider";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { PackInfo } from "./components/PackInfo";
 
 import Background from "../../Global-Components/Background";
 import Footer from "../../Global-Components/Footer";
 
 import styles from "./styles.module.scss";
+import Checkout from "../PackDetail/Components/Checkout";
+import Proccesing from "../PackDetail/Components/Proccesing";
+import Complete from "../PackDetail/Components/Complete";
+import { NftData } from "../../Context/NftProvider";
 
 const PackDetailV2 = () => {
   const [pack, setSelectedPack] = useState();
+  const [checkoutNCoin, setCheckoutNCoin] = useState(false);
+  const [processingNCoin, setProcessingNcoin] = useState(false);
+  const [buyComplete, setBuyComplete] = useState(false);
 
   const { setPack, packData } = useContext(PackData);
   const { packSelected } = useContext(PackData);
+  const { setReloadCollection } = useContext(NftData);
   const { id } = useParams();
+  const history = useHistory();
 
   useEffect(() => {
     const selectedPack = packData?.nftPackProducts?.find(
@@ -23,15 +32,47 @@ const PackDetailV2 = () => {
     setPack(selectedPack);
   }, [id, packData, setPack]);
 
+  const processingComplete = () => {
+    setCheckoutNCoin(false);
+    setProcessingNcoin(false);
+    setBuyComplete(true);
+  };
+
+  const handleCloseComplete = () => {
+    setReloadCollection((value) => !value);
+    history.push(`/open-pack`);
+  };
+
   return (
     <Background>
       <div className={styles.packDetail}>
         <div className={styles.header}>
           <img src={pack?.thumbnailUrl} alt="" />
         </div>
-        <PackInfo pack={packSelected} />
+        <PackInfo setCheckoutNCoin={setCheckoutNCoin} pack={packSelected} />
       </div>
       <Footer />
+      {checkoutNCoin && (
+        <Checkout
+          packBuy={packSelected}
+          nftProccesing={setProcessingNcoin}
+          handleClose={setCheckoutNCoin}
+        />
+      )}
+      {processingNCoin && (
+        <Proccesing
+          packBuy={packSelected}
+          processingComplete={processingComplete}
+          handleClose={() => setProcessingNcoin(false)}
+        />
+      )}
+      {buyComplete && (
+        <Complete
+          title={packSelected?.packName}
+          goOpenPack={() => handleCloseComplete()}
+          closeComplete={() => handleCloseComplete()}
+        />
+      )}
     </Background>
   );
 };
