b24b52caadf5412165311f0d53ecbccce8552e76
Mauro Vera <Maurovera069@gmail.com>
2022-09-26 14:59:23 -0300

    Add loader

--- src/Views/MarketplaceDetailV2/components/NftDetail/index.jsx
+++ src/Views/MarketplaceDetailV2/components/NftDetail/index.jsx
@@ -11,6 +11,7 @@ import Stats from "./components/Stats";
 import Abilities from "./components/Abilities";
 import P2eInfo from "./components/P2eInfo";
 import Generations from "./components/Generations";
+import Loader from "../../../../Global-Components/Loader";
 
 const NftDetail = () => {
   const history = useHistory();
@@ -50,18 +51,24 @@ const NftDetail = () => {
         <Link className={styles.goBackToMarket} to="/marketplace">
           &lt; Go back to Marketplace
         </Link>
-        <div className={styles.nft}>
-          <div className={styles.nftImg}>
-            <img src={chosenNft?.thumbnailUrl} alt={chosenNft?.itemName} />
+        {chosenNft?.thumbnailUrl ? (
+          <div className={styles.nft}>
+            <div className={styles.nftImg}>
+              <img src={chosenNft?.thumbnailUrl} alt={chosenNft?.itemName} />
+            </div>
+            <div className={styles.nftData}>
+              <Description chosenNft={chosenNft} />
+              {chosenNft?.type === 1 && <Stats chosenNft={chosenNft} />}
+              <Abilities chosenNft={chosenNft} />
+              <P2eInfo chosenNft={chosenNft} />
+              <Generations />
+            </div>
           </div>
-          <div className={styles.nftData}>
-            <Description chosenNft={chosenNft} />
-            {chosenNft?.type === 1 && <Stats chosenNft={chosenNft} />}
-            <Abilities chosenNft={chosenNft} />
-            <P2eInfo chosenNft={chosenNft} />
-            <Generations />
+        ) : (
+          <div className={styles.loadingContainer}>
+            <Loader />
           </div>
-        </div>
+        )}
       </div>
       {/* <Footer /> */}
     </>
