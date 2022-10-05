5429037757303765741062ad1a46ca252eb29ad7
Mauro Vera <Maurovera069@gmail.com>
2022-10-05 11:28:40 -0300

    Fix nft detail

--- src/Global-Components/NftDetail/components/P2eInfo/index.jsx
+++ src/Global-Components/NftDetail/components/P2eInfo/index.jsx
@@ -1,20 +1,14 @@
 import styles from "./styles.module.scss";
 import SERIAL from "../../../../Assets/img/Sprite_Icon_Premium_02.png";
-import BATTLECOUNT from "../../../../Assets/img/Sprite_Icon_Premium_03.png";
 import COPY from "../../../../Assets/img/Sprite_Icon_Premium_05.png";
-import DAILY from "../../../../Assets/img/Sprite_Icon_Reward_35.png";
 
 const P2eInfo = ({ chosenNft }) => {
-  console.log(chosenNft);
-
   return (
     <div className={styles.p2eInfo}>
       <h4>P2E Info</h4>
       <div className={styles.items}>
-        <Item img={DAILY} text={`Daily gNCoin Battle Count: ? / ?`} />
+        <Item img={COPY} text={"P2E Battle Count: ?"} />
         <Item img={SERIAL} text={"Serial Number: #?"} />
-        <Item img={BATTLECOUNT} text={"gNCoin Battle Count: ? / ?"} />
-        <Item img={COPY} text={"Copy: ?"} />
       </div>
     </div>
   );
