67310664e515c161a7441331f4b2d4a7d3d9fe36
Mauro Vera <Maurovera069@gmail.com>
2022-10-17 14:45:37 -0300

    Update nav

--- src/Global-Components/Navbar/components/ButtonNav/index.jsx
+++ src/Global-Components/Navbar/components/ButtonNav/index.jsx
@@ -2,10 +2,17 @@ import { Link } from "react-router-dom";
 
 import styles from "./styles.module.scss";
 
-export const ButtonNav = ({ text, onClick, to }) => {
+export const ButtonNav = ({ text, onClick, to, bgColor, textColor }) => {
   return (
     <Link to={to}>
-      <button className={styles.buttonNav} onClick={onClick}>
+      <button
+        style={{
+          backgroundColor: bgColor && bgColor,
+          color: bgColor && "white",
+        }}
+        className={styles.buttonNav}
+        onClick={onClick}
+      >
         <span>{text}</span>
       </button>
     </Link>
