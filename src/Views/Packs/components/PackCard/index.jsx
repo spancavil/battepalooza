a8d98da39eff8868cc31baf11f798df81dc4bff3
Mauro Vera <Maurovera069@gmail.com>
2022-11-07 16:56:19 -0300

    Connect by api

--- src/Views/Packs/components/PackCard/index.jsx
+++ src/Views/Packs/components/PackCard/index.jsx
@@ -1,35 +1,43 @@
-/* import { separator } from "../../../../Utils/separator"; */
 import NCOIN from "../../../../Assets/img/Sprite_Icon_Reward_35.png";
-import TESTPACK from "../../../../Assets/img/Packtest.png";
 import Timer from "../../../../Global-Components/Timer";
 
 import styles from "./styles.module.scss";
+import { separator } from "../../../../Utils/separator";
 
 export const PackCard = ({ pack, onClick }) => {
   return (
     <div onClick={onClick} className={styles.pack}>
       <div className={styles.packImgContainer}>
         <div className={styles.img} />
-        <img className={styles.packImg} src={TESTPACK} alt="pack" />
+        <img className={styles.packImg} src={pack?.thumbnailUrl} alt="pack" />
       </div>
       <div className={styles.packDataContainer}>
-        <h3>Test Pack</h3>
+        <h3>{pack?.packName}</h3>
         <p className={styles.desc}>
-          The power of the Slayer varies depending on the user. It is said that
-          in the past wars, warriors called war ghosts used them.
+          {pack?.detailTxt &&
+            pack?.detailTxt.split("\\n").map((texto) => {
+              return (
+                <p key={texto} className={styles.texto}>
+                  {texto}
+                </p>
+              );
+            })}
         </p>
         <span className={styles.left}>
           Left Time: <Timer pack={pack} />
         </span>
         <span className={styles.left}>
-          Left Count: <b>130/1000</b>
+          Left Count:
+          <b>
+            {pack?.leftAmount}/{pack?.limitAmount}
+          </b>
         </span>
         <div className={styles.priceContainer}>
           <p>Price</p>
           <div className={styles.price}>
             <img src={NCOIN} alt="icon" />
             <span>
-              <b>870,100</b> NCoin
+              <b>{separator(pack?.price)}</b> NCoin
             </span>
           </div>
         </div>
