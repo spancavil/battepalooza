22d10dccc69223c9a51050e42587c105c7bcadad
Mauro Vera <Maurovera069@gmail.com>
2022-11-08 11:57:35 -0300

    Add loaders, fix styles and errors

--- src/Views/PackDetailV3/components/Obtainable/index.jsx
+++ src/Views/PackDetailV3/components/Obtainable/index.jsx
@@ -1,16 +1,11 @@
+import Loader from "../../../../Global-Components/Loader";
 import { ObtainableNft } from "./components/ObtainableNft";
 import { PremiumBuffItems } from "./components/PremiumBuffItem";
 import { RarityItem } from "./components/RarityItem";
 
 import styles from "./styles.module.scss";
 
-const Obtainable = ({
-  pack,
-  setCheckoutNCoin,
-  nftList,
-  premiumBuffs,
-  rarityRates,
-}) => {
+const Obtainable = ({ nftList, premiumBuffs, rarityRates }) => {
   const getPercentages = () => {
     const rarities = rarityRates && Object.values(rarityRates);
 
@@ -35,31 +30,49 @@ const Obtainable = ({
   return (
     <div className={styles.obtainableContainer}>
       <div className={styles.left}>
-        <div className={styles.box}>
-          <h4>Rarity Rate</h4>
-          <div className={styles.rarityItems}>
-            {percentages?.map(({ percentage }, i) => (
-              <RarityItem percentage={percentage} rarity={i + 1} />
+        {percentages?.length > 0 ? (
+          <div className={styles.box}>
+            <h4>Rarity Rate</h4>
+            <div className={styles.rarityItems}>
+              {percentages?.map(({ percentage }, i) => (
+                <RarityItem key={i} percentage={percentage} rarity={i + 1} />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className={styles.boxLoading}>
+            <Loader />
+          </div>
+        )}
+        {premiumBuffs?.length > 0 ? (
+          <div className={styles.box}>
+            <h4>Obtainable Premium Buffs</h4>
+            <div className={styles.premiumBuffItems}>
+              {premiumBuffs?.map((item) => (
+                <PremiumBuffItems key={item?.id} item={item} />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className={styles.boxLoading}>
+            <Loader />
+          </div>
+        )}
+      </div>
+      {nftList?.length > 0 ? (
+        <div className={styles.rigth}>
+          <h4>Obtainable NFTs</h4>
+          <div className={styles.nftList}>
+            {nftList?.map((item) => (
+              <ObtainableNft key={item?.id} item={item} />
             ))}
           </div>
         </div>
-        <div className={styles.box}>
-          <h4>Obtainable Premium Buffs</h4>
-          <div className={styles.premiumBuffItems}>
-            {premiumBuffs?.map((item) => (
-              <PremiumBuffItems item={item} />
-            ))}
-          </div>
+      ) : (
+        <div className={styles.rigthLoading}>
+          <Loader />
         </div>
-      </div>
-      <div className={styles.rigth}>
-        <h4>Obtainable NFTs</h4>
-        <div className={styles.nftList}>
-          {nftList?.map((item) => (
-            <ObtainableNft item={item} />
-          ))}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
