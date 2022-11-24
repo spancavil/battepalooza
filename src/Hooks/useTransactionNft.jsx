4936e22f2a5a9b8ccb6f519fc78ea05eff684085
spancavil <soancavil@gmail.com>
2022-11-24 15:31:45 -0300

    Applied custom hook useTransaction in proccesing market and buy pack

--- src/Hooks/useTransactionNft.jsx
+++ src/Hooks/useTransactionNft.jsx
@@ -1,8 +1,202 @@
-import React from 'react'
+import { useContext, useEffect, useRef, useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { PackData } from "../Context/PackProvider";
+import { UserData } from "../Context/UserProvider";
+import dropService from "../Services/drop.service";
+import marketService from "../Services/market.service";
+import nftService from "../Services/nft.service";
+import { logOutAmplitude } from "../Utils/amplitude";
+import { fireAlertAsync } from "../Utils/sweetAlert2";
 
-const useTransactionNft = () => {
-    
-  return []
-}
+const useTransactionNft = (
+    { 
+        buyPack = false, 
+        buyMarket = false, 
+        burnNft = false,
+        quantity = 0,
+        packBuy = {},
+        nft = {},
+        processingComplete = () => {},
+        handleClose = () => {}
+    } = {},
+) => {
+    const [status, setStatus] = useState("");
+    const [error, setError] = useState("");
+    const [forteTxText, setForteTxText] = useState("");
+    const [step1, setStep1] = useState(true); //buyShop
+    const [step2, setStep2] = useState(false); //getBlockchainTxStatus
 
-export default useTransactionNft
+    const { userData } = useContext(UserData);
+    const { setTxResultPackBuy } = useContext(PackData);
+
+    const history = useHistory();
+
+    const lockedStep1 = useRef(false)
+
+    //Paso uno, hacemos la compra, y la API nos devuelve el Id de la tx
+    //para consultar en Forte
+    useEffect(() => {
+        //Buy pack or buy marketplace or burn
+        const functionStep1 = async () => {
+            //Solo entra en caso de estar en el paso 1
+            if (step1 && !lockedStep1.current) {
+                lockedStep1.current = true
+                console.log(buyMarket);
+                if (buyPack) console.log("Step 1. BuyShopNft");
+                if (buyMarket) console.log("Step 1. Buy marketplace");
+                if (burnNft) console.log("Step 1. Burn NFT");
+                if (Object.keys(userData).length !== 0) {
+                    try {
+                        let response;
+                        if (buyPack) {
+                            response = await dropService.buyShop(
+                                packBuy.id,
+                                userData.pid,
+                                quantity,
+                                userData.bpToken
+                            );
+                        }
+                        if (buyMarket) {
+                            response =
+                                await marketService.buyProductMarketplace(
+                                    userData.pid,
+                                    nft.sellerPid,
+                                    nft.uniqueId,
+                                    userData.bpToken
+                                );
+                        }
+                        if (burnNft) {
+                            response = await nftService.burnNft(
+                                userData.bpToken,
+                                nft.pid,
+                                nft.uuid
+                            );
+                        }
+                        console.log(response);
+                        if (response.error.text !== "") {
+                            if (response.error.text.includes("authorized")) {
+                                fireAlertAsync(
+                                    "Warning",
+                                    "Session expired, please login again."
+                                ).then(() => {
+                                    localStorage.removeItem("userBP");
+                                    logOutAmplitude();
+                                    history.push("/");
+                                });
+                            } else {
+                                setStatus("error");
+                                setError(response?.error?.text);
+                            }
+                            //Si no hay errores seteamos el forteTxId para el paso 2.
+                        } else {
+                            setForteTxText(response.forteTxId);
+                            setStep1(false);
+                            setStep2(true);
+                        }
+                    } catch (error) {
+                        setStatus("error");
+                        setError(error?.message);
+                    }
+                    lockedStep1.current = false
+                }
+            }
+        };
+
+        functionStep1();
+    }, [
+        userData, 
+        setForteTxText, 
+        history, 
+        packBuy, 
+        step1, 
+        quantity,
+        buyMarket,
+        burnNft,
+        buyPack,
+        nft
+    ]);
+
+    //Paso 2
+    //Con el id de la tx vamos haciendo la consulta del status de la operación en la blockchain.
+    useEffect(() => {
+        let forteStatusInterval;
+        if (forteTxText !== "" && Object.keys(userData).length !== 0 && step2) {
+            console.log(`Step 2. Get forte tx`);
+            console.log(`Forteid: ${forteTxText}, Status: ${status}`);
+
+            const getStatusForte = async () => {
+                try {
+                    const response = await dropService.getTransactionStatus(
+                        userData.pid,
+                        forteTxText,
+                        userData.bpToken
+                    );
+                    if (response.error.text !== "") {
+                        if (response.error.text.includes("authorized")) {
+                            fireAlertAsync(
+                                "Warning",
+                                "Session expired, please login again."
+                            ).then(() => {
+                                localStorage.removeItem("userBP");
+                                logOutAmplitude();
+                                history.push("/");
+                                window.location.reload();
+                            });
+                        } else {
+                            setStatus("error");
+                            setError(response?.error?.text);
+                            setTimeout(() => {
+                                handleClose();
+                            }, 3000);
+                        }
+                    } else {
+                        //Response OK, no errors
+                        //console.log(`Status on proccessing: ${response.status}`);
+                        setStatus(response.status);
+                        if (response.status === "completed") {
+                            console.log(response);
+                            setTxResultPackBuy(response?.txResult);
+                        }
+                    }
+                } catch (error) {
+                    setStatus("error");
+                    setError(error?.message);
+                    setTimeout(() => {
+                        handleClose();
+                    }, 3000);
+                }
+            };
+
+            //We call the status of the transaction in forte each 0.5 secs
+            forteStatusInterval = setInterval(getStatusForte, 500);
+            setTimeout(() => {
+                if (status !== "pending" && status !== "") {
+                    clearInterval(forteStatusInterval);
+                    if (step2 && status === "completed") {
+                        setTimeout(() => {
+                            processingComplete();
+                        }, 3000);
+                    }
+                }
+            }, 1000);
+        }
+        //Interval clear at component will unmount
+        return () => {
+            clearInterval(forteStatusInterval);
+        };
+    }, [
+        forteTxText,
+        userData,
+        history,
+        status,
+        handleClose,
+        processingComplete,
+        step2,
+        setTxResultPackBuy,
+        error?.message,
+    ]);
+
+    return [status, error];
+};
+
+export default useTransactionNft;
--- src/Views/MarketplaceDetailV2/components/Proccesing/index.jsx
+++ src/Views/MarketplaceDetailV2/components/Proccesing/index.jsx
@@ -1,160 +1,44 @@
-import React, { useContext, useEffect, useState } from "react";
-import { UserData } from "../../../../Context/UserProvider";
-import marketService from "../../../../Services/market.service";
+import React from "react";
 import styles from "./styles.module.scss";
-import { logOutAmplitude } from "../../../../Utils/amplitude";
-import { useHistory } from "react-router-dom";
-import { fireAlertAsync } from "../../../../Utils/sweetAlert2";
+import useTransactionNft from "../../../../Hooks/useTransactionNft";
 import ModalV2 from "../../../../Global-Components/ModalV2";
 import ButtonAnimated from "../../../../Global-Components/ButtonAnimated";
-import useTransactionNft from "../../../../Hooks/useTransactionNft";
 
 const Proccesing = ({ nftBuy, handleClose, proccessingComplete }) => {
-  /* const [status, setStatus] = useState("");
-  const [error, setError] = useState("");
-  const [forteTxText, setForteTxText] = useState("");
+    const [status, error] = useTransactionNft({
+        buyMarket: true,
+        nft: nftBuy,
+        processingComplete: proccessingComplete,
+        handleClose: handleClose,
+    });
 
-  const { userData } = useContext(UserData);
+    console.log(status, error);
 
-  const history = useHistory(); */
-
-  const [status, error, forteTxText] = useTransactionNft(
-    {buyMarket: true},
-    0,
-    {},
-    nftBuy,
-    proccessingComplete,
-    handleClose
-  )
-
-  //Paso uno, hacemos la compra, y forte nos devuelve el Id de la tx
-/*   useEffect(() => {
-    const buyNft = async () => {
-      console.log("Trata de hacer la compra");
-      //Si el forteTxText es distinto a "" significa que no tenemos que hacer la compra nuevamente
-      if (status === "") {
-        if (Object.keys(userData).length !== 0) {
-          try {
-            const response = await marketService.buyProductMarketplace(
-              userData.pid,
-              nftBuy.sellerPid,
-              nftBuy.uniqueId,
-              userData.bpToken
-            );
-            if (response.error.text !== "") {
-              if (response.error.text.includes("authorized")) {
-                fireAlertAsync(
-                  "Warning",
-                  "Session expired, please login again."
-                ).then(() => {
-                  localStorage.removeItem("userBP");
-                  logOutAmplitude();
-                  history.push("/");
-                  //window.location.reload();
-                });
-              } else {
-                setStatus("error");
-                setError(response?.error?.text);
-              }
-              //No errors, the forte transaction text id is returned. With that text
-              //we call the transaction status in the next step (modal register 2)
-            } else {
-              setForteTxText(response.forteTxId);
-            }
-          } catch (error) {
-            setStatus("error");
-            setError(error?.message);
-          }
-        }
-      }
-    };
-
-    buyNft();
-  }, [userData, setForteTxText, history, nftBuy, handleClose, status]); */
-
-  //Paso 2, con el id de la tx vamos haciendo la consulta del status de la operación
-  /* useEffect(() => {
-    let forteStatusInterval;
-    if (forteTxText !== "" && Object.keys(userData).length !== 0) {
-      console.log(`Forteid: ${forteTxText}, Status: ${status}`);
-
-      const getStatusForte = async () => {
-        try {
-          const response = await marketService.getTransactionStatus(
-            userData.pid,
-            forteTxText,
-            userData.bpToken
-          );
-          if (response.error.text !== "") {
-            if (response.error.text.includes("authorized")) {
-              fireAlertAsync(
-                "Warning",
-                "Session expired, please login again."
-              ).then(() => {
-                localStorage.removeItem("userBP");
-                logOutAmplitude();
-                history.push("/");
-                window.location.reload();
-              });
-            } else {
-              setStatus("error");
-              setError(response?.error?.text);
-            }
-          } else {
-            //Response OK, no errors
-            setStatus(response.status);
-          }
-        } catch (error) {
-          setStatus("error");
-          setError(error?.message);
-        }
-      };
-
-      //We call the status of the transaction in forte each 0.5 secs
-      forteStatusInterval = setInterval(getStatusForte, 500);
-      if (status !== "pending" && status !== "") {
-        clearInterval(forteStatusInterval);
-        setTimeout(() => {
-          proccessingComplete();
-        }, 5000);
-      }
-    }
-    //Interval clear at component will unmount
-    return () => {
-      clearInterval(forteStatusInterval);
-    };
-  }, [
-    forteTxText,
-    userData,
-    history,
-    status,
-    handleClose,
-    proccessingComplete,
-  ]); */
-
-  return (
-    <div className={styles.parentContainerModal}>
-      <ModalV2
-        title={
-          status !== "error" && status !== "completed" ? "Processing" : status
-        }
-      >
-        <h3 className={styles.textDrop}>
-          {status !== "error" ? (
-            <>
-              {nftBuy?.packName} is being transferred. Please wait while the
-              transfer is being completed
-            </>
-          ) : (
-            <>{error}</>
-          )}
-        </h3>
-        <div className={styles.buttonContainer}>
-          <ButtonAnimated content={status} onClick={handleClose} />
+    return (
+        <div className={styles.parentContainerModal}>
+            <ModalV2
+                title={
+                    status !== "error" && status !== "completed"
+                        ? "Processing"
+                        : status
+                }
+            >
+                <h3 className={styles.textDrop}>
+                    {status !== "error" ? (
+                        <>
+                            {nftBuy?.packName} is being transferred. Please wait
+                            while the transfer is being completed
+                        </>
+                    ) : (
+                        <>{error}</>
+                    )}
+                </h3>
+                <div className={styles.buttonContainer}>
+                    <ButtonAnimated content={status} onClick={handleClose} />
+                </div>
+            </ModalV2>
         </div>
-      </ModalV2>
-    </div>
-  );
+    );
 };
 
 export default Proccesing;
